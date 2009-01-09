920c8e2d509d09423a89328c92ba1518a470f2f2
Sergio Pascual <sergiopr@fis.ucm.es>
2009-01-09 18:20:56 +0000

    A graphic view of the combination process using matplotlib. Refs #40

--- src/emir/image/__init__.py
+++ src/emir/image/__init__.py
@@ -1,6 +1,7 @@
 # $Id$
 
 import time
+import _combine
 def print_timing(func):
     '''Timing decorator'''
     def wrapper(*arg, **keywords):
@@ -11,12 +12,8 @@ def print_timing(func):
         print '%s took %0.3f s' % (func.func_name, (t2 - t1))
         return res
     return wrapper
-    
-def test_conversion():
-    import _combine
-    import numpy
-    
-    def fun(data):
+
+def fun(data):
         '''This is a example function that can be used with _combine.test1
         
         It takes a list of values and returns a tuple with (value, variance, number of points used)
@@ -32,38 +29,77 @@ def test_conversion():
         v = s2 / (l - 1) - (s * s) / (l * (l - 1));
         return (s / l, v, l)
     
-    @print_timing
-    def ucombine1(method, images, masks, res = None):
-        return _combine.test1(method, images, masks, res)
+@print_timing
+def ucombine1(method, images, masks, res=None, var=None, num=None):
+    return _combine.test1(method, images, masks, res, var, num)
     
-    @print_timing
-    def ucombine2(images, masks, res = None):
-        return _combine.test2(images, masks, res)
+@print_timing
+def ucombine2(images, masks, res, var, num):
+    return _combine.test2(images, masks, res, var, num)
     
+def test1():
+    
+    import numpy
     shape = (10, 10)
     num = 10
     images = [numpy.ones(shape) * i  for i in xrange(num)]
-    simages = [i[0:5,0:5]  for i in images]
+    simages = [i[0:5, 0:5]  for i in images]
     res = numpy.zeros(shape)
     ros = numpy.zeros(shape)
     mask = numpy.ones(shape, dtype='bool')
     #out1 = ucombine1(fun, images, [mask] * num)
-    ucombine1(_combine.method1, simages, [mask[0:5,0:5]] * num, res=res[0:5,0:5])
-    ucombine2(simages, [mask[0:5,0:5]] * num, res=ros[0:5,0:5])
+    ucombine1(_combine.method1, simages, [mask[0:5, 0:5]] * num, res=res[0:5, 0:5])
+    ucombine2(simages, [mask[0:5, 0:5]] * num, res=ros[0:5, 0:5])
     print res
     print ros
-    simages = [i[5:10,5:10]  for i in images]
-    ucombine1(_combine.method1, simages, [mask[5:10,5:10]] * num, res=res[5:10,5:10])
-    ucombine2(simages, [mask[5:10,5:10]] * num, res=ros[5:10,5:10])
+    simages = [i[5:10, 5:10]  for i in images]
+    ucombine1(_combine.method1, simages, [mask[5:10, 5:10]] * num, res=res[5:10, 5:10])
+    ucombine2(simages, [mask[5:10, 5:10]] * num, res=ros[5:10, 5:10])
     print res
     print ros
     
+def test2(shape, num):
+    import _combine
+    import numpy
+    import matplotlib
+    matplotlib.use('GTKAgg')
+    import matplotlib.pyplot as plt
     
     
-
-def test():
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
     
+    images = [numpy.ones(shape) * i  for i in xrange(num)]
+    #numdisplay.display(images[0])
     
+    row_interval = 100
+    resf = images[0].copy()
+    varf = images[0].copy()
+    numf = images[0].copy().astype('int')
+    mask = numpy.ones(shape, dtype='bool')
+    
+    def animate():    
+        slice1 = slice(0,shape[0])
+        slice2 = None
+        sup = ax.imshow(resf)
+        fig.canvas.draw()
+        for trow in xrange(0, shape[1], row_interval):
+          slice2 = slice(trow, min(trow + row_interval, shape[0]))
+          subimages = [i[(slice2,slice1)] for i in images]
+          submasks = [mask[(slice2, slice1)] for i in images]
+          ucombine2(subimages, submasks, resf[(slice2,slice1)], varf[(slice2,slice1)], numf[(slice2,slice1)])
+          #sup.set_data(resf)
+          sup = ax.imshow(resf)
+          fig.canvas.draw()
+          
+        time.sleep(5)
+        raise SystemExit
+      
+    import gobject
+    gobject.idle_add(animate)
+    plt.show()
+        
+def test3():    
     import _combine
     import numpy
     import numpy.random
@@ -153,4 +189,4 @@ def test():
     print a[2][0, 0]
     
 if __name__ == "__main__":
-    test_conversion()
+    test2((2048,2048), 10)
