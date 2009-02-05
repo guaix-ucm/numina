ca311440079b207d0e37f67fd7222c00be340151
Sergio Pascual <sergiopr@fis.ucm.es>
2009-02-05 12:16:47 +0000

    Fixed SlitProfile, changed the internals of *Profile classes and add_profile

--- lib/emir/simulation/__init__.py
+++ lib/emir/simulation/__init__.py
@@ -17,7 +17,7 @@
 # along with PyEmir.  If not, see <http://www.gnu.org/licenses/>.
 # 
 
-# $Id: detector.py 386 2009-01-20 18:10:57Z spr $
+# $Id$
 
 import numpy
 import math
@@ -25,98 +25,102 @@ from scipy.special import erf
 from scipy import maximum, minimum
 from emir.image import subarray_match
 
-M_SQRT1_2 = math.sqrt(1. / 2.)
-
-class GaussProfile:
-  def __init__(self, sigma, fcenter= (0.,0.), scale = 5):
-    self.sigma = sigma
-    self.fcenter = fcenter
-    self.scale = scale
-  def side(self):
-    l = int(round(self.scale * self.sigma))
-    return (l, l)
-  def shape(self):
-    l = 2 * int(round(self.scale * self.sigma)) + 1
-    return (l, l)
-  def area(self, x, y):
-    def compute1(v, center, sigma):
-      return M_SQRT1_2 * (v - center) / sigma
-    def compute2(v, center, sigma):
-      tipV1 = compute1(v, center, sigma)
-      tipV2 = compute1(v + 1, center, sigma)
-      return 0.5 * (erf(tipV2) - erf(tipV1))
-      
-    side = self.side()
-    return compute2(x, self.fcenter[1] + side[1] + 1, self.sigma) * compute2(y, self.fcenter[0] + side[0] + 1,
- self.sigma)
-  def array(self):
-      y, x = numpy.indices(self.shape())
-      return self.area(x, y)
-
-class SlitProfile:
-    def __init__(self, sides, center = (0.5, 0.5)):
-        self.blc = numpy.array([0,0])        
-        self.urc = numpy.array(sides, dtype='int') + 1
-    def shape(self):
-        return (int(self.urc[0]) - int(self.blc[0]) + 1, int(self.urc[1]) - int(self.blc[1]) + 1)
-    def area(self, x, y):
-        return (minimum(x + 1, self.urc[1]) - maximum(x, self.blc[1])) * (minimum(y + 1, self.urc[0]) - maximum(y, self.blc[0]))
-    def set_center(self, center):
-        self.blc = numpy.array([center[0] - sides[0] / 2. , center[1] - sides[1] / 2.], dtype='int')        
-        self.urc = numpy.array([center[0] + sides[0] / 2. + 1 , center[1] + sides[1] / 2. + 1], dtype='int')        
-    def get_center(self):
-        return ((self.blc[0] + self.urc[0]) / 2.,(self.blc[1] + self.urc[1]) / 2.)
-    center = property(get_center, set_center)
+class Profile:
+    '''Base class for 2D profiles'''
+    def __init__(self, shape, offset):
+        self.shape = shape
+        # Offset corresponds to the point [0,0] of array
+        self.offset = offset
     def array(self):
-      print self.shape()
-      print self.urc
-      print self.blc
-      y, x = numpy.indices(self.shape())
-      return self.area(x, y)
+        y, x = numpy.indices(self.shape)
+        return self.area(x, y)
+    def area(self, x ,y):
+        return numpy.ones(self.shape)
 
+class GaussProfile(Profile):
+    '''Simetrical gaussian profile'''
+    M_SQRT1_2 = math.sqrt(1. / 2.) 
+    def __init__(self, sigma, center, scale = 5):       
+        self.center = numpy.array(center)
+        self.sigma = sigma
+        l = int(round(scale * sigma))
+        # Offset corresponds to the point [0,0] of array
+        offset = self.center.astype('int') - numpy.array([l,l]) 
+        Profile.__init__(self, shape=(2 * l + 1, 2 * l + 1), offset=offset)
+    @staticmethod
+    def __compute1(v, center, sigma):
+        return GaussProfile.M_SQRT1_2 * (v - center) / sigma
+    @staticmethod
+    def __compute2(v, center, sigma):
+        tipV1 = GaussProfile.__compute1(v, center, sigma)
+        tipV2 = GaussProfile.__compute1(v + 1, center, sigma)
+        return 0.5 * (erf(tipV2) - erf(tipV1))
+    def area(self, x, y):      
+        v = GaussProfile.__compute2(x, self.center[1] - self.offset[1], self.sigma)
+        w = GaussProfile.__compute2(y, self.center[0] - self.offset[0], self.sigma)
+        return  v * w
 
-def add_profile(im, center0, center1, profile, intensity=1000.):
-  center = numpy.array([center0, center1])
-  rcenter = map(int, center)
-  pcenter = center - rcenter
-  profile.center = pcenter
-  ss = profile.array()
-  i,j = subarray_match(im.shape, rcenter, ss.shape)
-  im[i] += intensity * ss[j]
-  return im
+class SlitProfile(Profile):
+    ''' A rectangular slit'''
+    def __init__(self, blc, urc):
+        self.urc = numpy.array(urc)
+        self.blc = numpy.array(blc)
+        self.eurc = self.urc.astype('int')
+        self.eblc = self.blc.astype('int')
+        self.lurc = self.urc - self.eblc
+        self.lblc = self.blc - self.eblc
+        Profile.__init__(self, shape=tuple(self.eurc - self.eblc + 1), offset=self.eblc)
+    def area(self, x, y):                
+        v = minimum(x + 1, self.lurc[1]) - maximum(x, self.lblc[1])
+        w = minimum(y + 1, self.lurc[0]) - maximum(y, self.lblc[0])
+        return v * w
+
+def add_profile(im, profile, intensity=1000.):
+    '''Adds the array given by profile.array() in the position profile.offset 
+    multiplied by value of intensity'''
+    ss = profile.array()
+    i,j = subarray_match(im.shape, profile.offset, ss.shape)
+    im[i] += intensity * ss[j]
+    return im
+
+def add_gaussian(im, sigma, center, scale = 5, intensity=1000.):
+    '''Adds a Gaussian profile using add_profile'''
+    profile = GaussProfile(sigma=sigma, center = center, scale=scale)
+    return add_profile(im, profile, intensity=intensity)
 
 if __name__ == "__main__":
     from numdisplay import display
+    
     def fun1():
         im = numpy.random.normal(loc=1000., scale=1.0, size=(1000,1000))
         numpy.random.seed(250)
         npos = 1000
         y = numpy.random.uniform(high=1000,size=npos)
         x = numpy.random.uniform(high=1000,size=npos)
-        profile = [GaussProfile(1.4)] * npos
-        for i in zip(y,x,profile):
-          subref = add_profile(im, *i)
+        lx = 50.
+        ly = 12.
+        for i in zip(y,x):
+            profile = GaussProfile(sigma=1.4, center = i)
+            #blc = (i[0] - ly / 2, i[1] - lx / 2)
+            #urc = (i[0] + ly / 2, i[1] + lx / 2)
+            #profile = SlitProfile(blc, urc)
+            subref = add_profile(im, profile)
         display(subref)
+    
     def fun2():
-        
         im = numpy.random.normal(loc=1000., scale=1.0, size=(1000,1000))
         numpy.random.seed(250)
-        npos = 50
+        npos = 1000
         y = numpy.random.uniform(high=1000,size=npos)
-        x = numpy.arange(0,1000,20)
-        profile = [SlitProfile((15.5, 23.8))] * npos
-        for i in zip(y,x,profile):
-          subref = add_profile(im, *i)
+        x = numpy.random.uniform(high=1000,size=npos)
+        lx = 50.
+        ly = 12.
+        for i in zip(y,x):            
+            blc = (i[0] - ly / 2, i[1] - lx / 2)
+            urc = (i[0] + ly / 2, i[1] + lx / 2)
+            profile = SlitProfile(blc, urc)
+            subref = add_profile(im, profile)
         display(subref)
-        #p = SlitProfile((15, 23))
-        #display(p.array())
-    def fun3(): 
-        p = GaussProfile(sigma=5, fcenter=(0.5, 0.5))
-        im = numpy.random.normal(loc=1000., scale=1.0, size=(1000,1000))
-        a = p.array()
-        ain, bin = subarray_match(im.shape, (990, 25), a.shape)
-        im[ain] -= 2000 * a[bin] 
-        display(im)
-        add_profile(im, 45.89, 95.99, p, intensity=1000.)
-        
+    
+    fun1()
     fun2()
