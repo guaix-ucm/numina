84cf49d307eb3eeb3fb7015cedca7a168bd2ae2c
Sergio Pascual <sergiopr@fis.ucm.es>
2010-01-07 10:36:11 +0000

    Creating numina.instrument subpackage

---
+++ lib/numina/instrument/__init__.py
@@ -0,0 +1,20 @@
+#
+# Copyright 2008-2010 Sergio Pascual
+# 
+# This file is part of PyEmir
+# 
+# PyEmir is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# PyEmir is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with PyEmir.  If not, see <http://www.gnu.org/licenses/>.
+# 
+
+# $Id: __init__.py 410 2009-03-06 19:12:32Z spr $
