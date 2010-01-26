8dc016a215f2cd418410d9fd7cf60884a7d3f37a
Sergio Pascual <sergiopr@fis.ucm.es>
2010-01-26 23:06:57 +0000

    Release 0.0.4

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ ex1 = Extension('numina.image._combine',
           include_dirs=[numpy_include])
 
 setup(name='pyemir',
-      version='0.0.3',
+      version='0.0.4',
       author='Sergio Pascual',
       author_email='sergiopr@astrax.fis.ucm.es',
       url='http://guaix.fis.ucm.es/projects/emir',
-      download_url='ftp://astrax.fis.ucm.es/pub/users/spr/emir/pyemir-0.0.3.tar.gz',
+      download_url='ftp://astrax.fis.ucm.es/pub/users/spr/emir/pyemir-0.0.4.tar.gz',
       license='GPLv3',
       description='EMIR Data Processing Pipeline',
       long_description='EMIR Data Processing Pipeline',
