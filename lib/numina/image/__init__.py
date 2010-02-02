ec9bbfba69bdc787a2942b9d51e4da08640c086e
Sergio Pascual <sergiopr@fis.ucm.es>
2010-02-02 16:21:50 +0000

    subarray_match works with arbitrary shapes, fixes #46

--- lib/numina/image/__init__.py
+++ lib/numina/image/__init__.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2008-2009 Sergio Pascual
+# Copyright 2008-2010 Sergio Pascual
 # 
 # This file is part of PyEmir
 # 
@@ -21,39 +21,58 @@
 
 __version__ = "$Revision$"
 
-import numpy
-from scipy import maximum, minimum
-
-from numina.exceptions import Error
+import scipy as sc
 
 
-
-def subarray_match(shape, ref, sshape, sref=(0, 0)):
-    '''Matches two arrays, given the different shapes given by shape and shape, and a reference point ref
+def subarray_match(shape, ref, sshape, sref=None):
+    '''Compute the slice representation of intersection of two arrays.
+    
+    Given the shapes of two arrays and a reference point ref, compute the
+    intersection of the two arrays.
     It returns a tuple of slices, that can be passed to the two images as indexes
-      
+    
+    :param shape: the shape of the reference array
+    :param ref: coordinates of the reference point in the first array system
+    :param sshape: the shape of the second array
+    :param: sref: coordinates of the reference point in the second array system, the origin by default
+    :type sref: sequence or None
+    :return: two matching slices, corresponding to both arrays or a tuple of Nones if they don't match
+    :rtype: a tuple 
+    
     Example: 
     
-      >>> im = numpy.zeros((1000,1000))
+      >>> im = numpy.zeros((1000, 1000))
       >>> sim = numpy.ones((40, 40))
       >>> i,j = subarray_match(im.shape, [20, 23], sim.shape)
       >>> im[i] = 2 * sim[j]
     
     '''
     # Reference point in im
-    ref1 = numpy.array(ref, dtype='int')  
-    # Ref2 is the reference point in sim  
-    # The pixel [0,0] of the array by default
-    ref2 = numpy.array(sref)   
+    ref1 = sc.asarray(ref, dtype='int')  
+    
+    if sref is not None:
+        ref2 = sc.asarray(sref, dtype='int')
+    else:
+        ref2 = sc.zeros_like(ref1) 
+    
     offset = ref1 - ref2    
-    urc1 = minimum(offset + numpy.array(sshape) - 1, numpy.array(shape) - 1)
-    blc1 = maximum(offset, 0)
+    urc1 = sc.minimum(offset + sc.asarray(sshape) - 1, sc.asarray(shape) - 1)
+    blc1 = sc.maximum(offset, 0)
     urc2 = urc1 - offset
     blc2 = blc1 - offset
-    return ((slice(blc1[0], urc1[0] + 1), slice(blc1[1], urc1[1] + 1)),
-            (slice(blc2[0], urc2[0] + 1), slice(blc2[1], urc2[1] + 1)))
-
- 
-
+    
+    def valid_slice(b, u):
+        if b >= u + 1:
+            return None
+        else:
+            return slice(b, u + 1)
+    
+    f = tuple(valid_slice(b, u) for b, u in zip(blc1, urc1))
+    s = tuple(valid_slice(b, u) for b, u in zip(blc2, urc2)) 
+    
+    if not all(f) or not all(s):
+        return (None, None)
+    
+    return (f, s)
 
 
