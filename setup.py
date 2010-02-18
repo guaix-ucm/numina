d370679c8018a18c6ac853f56889f28dcb932831
Sergio Pascual <sergiopr@fis.ucm.es>
2010-02-18 11:24:21 +0000

    Release 0.2.0 of PyEmir. Relates #85

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ ex1 = Extension('numina.array._combine',
           include_dirs=[numpy_include])
 
 setup(name='pyemir',
-      version='0.1.0',
+      version='0.2.0',
       author='Sergio Pascual',
       author_email='sergiopr@fis.ucm.es',
       url='http://guaix.fis.ucm.es/projects/emir',
-      download_url='ftp://astrax.fis.ucm.es/pub/users/spr/emir/pyemir-0.1.0.tar.gz',
+      download_url='ftp://astrax.fis.ucm.es/pub/users/spr/emir/pyemir-0.2.0.tar.gz',
       license='GPLv3',
       description='EMIR Data Processing Pipeline',
       packages=find_packages('lib'),
