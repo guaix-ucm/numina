5cd208cabfa876f91e7e42c3ec84891e2646c60d
Sergio Pascual <sergiopr@fis.ucm.es>
2010-06-07 13:04:38 +0200

    Backporting logging - warnings integration from python 2.7

--- lib/numina/logger.py
+++ lib/numina/logger.py
@@ -21,6 +21,7 @@
 '''Extra logging handlers for the numina logging system'''
 
 import logging
+import warnings
 
 class NullHandler(logging.Handler):
     '''NullHandler is the default log handler of the package. It 
@@ -28,3 +29,43 @@ class NullHandler(logging.Handler):
     def emit(self, record):
         '''Do-nothing method'''
         pass
+
+# Warnings integration
+# Backported from python 2.7
+
+_warnings_showwarning = None
+
+def _showwarning(message, category, filename, lineno, file=None, line=None):
+    """
+    Implementation of showwarnings which redirects to logging, which will first
+    check to see if the file parameter is None. If a file is specified, it will
+    delegate to the original warnings implementation of showwarning. Otherwise,
+    it will call warnings.formatwarning and will log the resulting string to a
+    warnings logger named "py.warnings" with level logging.WARNING.
+    """
+    if file is not None:
+        if _warnings_showwarning is not None:
+            _warnings_showwarning(message, category, filename, lineno, file, line)
+    else:
+        s = warnings.formatwarning(message, category, filename, lineno, line)
+        logger = logging.getLogger("py.warnings")
+        if not logger.handlers:
+            logger.addHandler(NullHandler())
+        logger.warning("%s", s)
+
+def captureWarnings(capture):
+    """
+    If capture is true, redirect all warnings to the logging package.
+    If capture is False, ensure that warnings are not redirected to logging
+    but to their original destinations.
+    """
+    global _warnings_showwarning
+    if capture:
+        if _warnings_showwarning is None:
+            _warnings_showwarning = warnings.showwarning
+            warnings.showwarning = _showwarning
+    else:
+        if _warnings_showwarning is not None:
+            warnings.showwarning = _warnings_showwarning
+            _warnings_showwarning = None
+
