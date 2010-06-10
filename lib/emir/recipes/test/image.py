5d9b3a8989d796480ecb74b0a5c9ff95c701cb74
Sergio Pascual <sergiopr@fis.ucm.es>
2010-06-10 13:43:18 +0200

    Simplified EmirImage

--- lib/emir/recipes/test/image.py
+++ lib/emir/recipes/test/image.py
@@ -4,23 +4,6 @@ import numpy
 
 from numina.array.combine import median
 
-class ObservingMode(object):
-    pass
-
-class ObservingBlock(object):
-    def __init__(self, images):
-        self.images
-        self.dither_pattern
-
-class Calibration(object):
-    pass
-
-
-class FitsHandle(object):
-    def __init__(self):
-        pass
-
-
 class EmirImage(object):
     def __init__(self, datafile, maskfile, offset=None):
         super(EmirImage, self).__init__()
@@ -29,22 +12,21 @@ class EmirImage(object):
         self.offset = offset
         self.noffset = None
         self.region = None
-        self.proc = False
         self.meta = None
         self.datafile = datafile
         self.maskfile = maskfile
         self._open = False
-        self.hdulist = []
+        self.hdulist = [None, None]
 
     def is_open(self):
         return self._open
 
     def open(self, mode='copyonwrite', memmap=False):
         self._open = True
-        self.hdulist.append(pyfits.open(self.datafile, memmap=memmap, mode=mode))
+        self.hdulist[0] = pyfits.open(self.datafile, memmap=memmap, mode=mode)
         self.data = self.hdulist[0]['primary'].data
         self.meta = self.hdulist[0]['primary'].header
-        self.hdulist.append(pyfits.open(self.maskfile, memmap=memmap, mode=mode))
+        self.hdulist[1] = pyfits.open(self.maskfile, memmap=memmap, mode=mode)
         self.mask = self.hdulist[-1]['primary'].data
 
     def close(self, output_verify='exception'):
@@ -55,14 +37,28 @@ class EmirImage(object):
         self._open = False
         self.data = None
         self.mask = None
+        self.hdulist = [None, None]
+        
+    def __copy__(self):
+        new = EmirImage(datafile=self.datafile, maskfile=self.maskfile)
+        new.offset = self.offset
+        new.noffset = self.noffset
+        new.region = self.region
+        return new
 
     def __str__(self):
-        return 'EmirImage(datafile=%s, maskfile=%s)' % (self.datafile, self.maskfile)
+        return 'EmirImage(datafile="%s", maskfile="%s")' % (self.datafile, self.maskfile)
 
-def combine(mode, images, scales=None, *args, **kwds):
-    data = [i.data for i in images]
-    masks = [i.mask for i in images]
-    
-    superflat = median(data, masks, scales=scales)
-
+def combine(mode, images, *args, **kwds):
+    data = [i.data[i.region] for i in images]
+    masks = [i.mask[i.region] == 0 for i in images]
+    superflat = median(data, masks, *args, **kwds)
     return superflat
+
+def combine2(mode, images, *args, **kwds):
+    data = [i.data[i.region] for i in images]
+    #masks = [i.mask[i.region] != 0 for i in images]
+    superflat = median(data)
+    return superflat
+
+
