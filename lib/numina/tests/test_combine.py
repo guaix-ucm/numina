5272ed20cad79e6865880222787f4b6fdc942fae
Sergio Pascual <sergiopr@fis.ucm.es>
2010-07-28 18:56:28 +0200

    Simple test cases for mimax rejection

--- lib/numina/tests/test_combine.py
+++ lib/numina/tests/test_combine.py
@@ -22,10 +22,11 @@ import unittest
 import numpy
 
 from numina.array.combine import assign_def_values
-from numina.array.combine import mean
+from numina.array.combine import mean, combine
 from numina.array.combine import CombineError
 
 class AssignDefValuesTestCase(unittest.TestCase):
+    '''Test case for assign_def_values.'''
     def testValues(self):
         '''Test assign_def_values.'''
         self.assertEqual(assign_def_values((), []), ())
@@ -36,13 +37,13 @@ class AssignDefValuesTestCase(unittest.TestCase):
         self.assertEqual(assign_def_values((5,), [('par1', 1)]), (5,))
         self.assertRaises(ValueError, assign_def_values, (8, 9), [('par1', 1)])
         
-        self.assertEqual(assign_def_values((), 
+        self.assertEqual(assign_def_values((),
                           [('par1', 1), ('par2', 2)]), (1, 2))
-        self.assertEqual(assign_def_values((5,), 
+        self.assertEqual(assign_def_values((5,),
                           [('par1', 1), ('par2', 2)]), (5, 2))
-        self.assertEqual(assign_def_values((8, 9), 
+        self.assertEqual(assign_def_values((8, 9),
                           [('par1', 1), ('par2', 2)]), (8, 9))
-        self.assertRaises(ValueError, assign_def_values, (8, 9, 10), 
+        self.assertRaises(ValueError, assign_def_values, (8, 9, 10),
                           [('par1', 1), ('par2', 2)])
         
         
@@ -81,24 +82,24 @@ class CombineTestCase(unittest.TestCase):
         input3 = numpy.array([[7, 2, 1, 4], [1, 2, 0, 4], [44, 2, 2, 0]])
         inputs = [input1, input2, input3]
         
-        mask1 = numpy.array([[False, False, False, True], 
-                             [False, True, False, False], 
+        mask1 = numpy.array([[False, False, False, True],
+                             [False, True, False, False],
                              [False, True, False, False]])
-        mask2 = numpy.array([[False, False, False, False], 
-                             [False, True, False, False], 
+        mask2 = numpy.array([[False, False, False, False],
+                             [False, True, False, False],
                              [False, True, False, False]])
-        mask3 = numpy.array([[False, False, False, False], 
-                             [False, True, False, False], 
+        mask3 = numpy.array([[False, False, False, False],
+                             [False, True, False, False],
                              [False, False, True, False]])
         masks = [mask1, mask2, mask3]
-        rres = numpy.array([[3.66666667, 2., 4., 4.], 
+        rres = numpy.array([[3.66666667, 2., 4., 4.],
                             [2.6666666666666665, 0., 1. , 4.],
                             [18., 2., 1.5 , 2.66666667]])
         rvar = numpy.array([[6.22222222222, 0.0, 8.66666666667, 0.0],
                             [5.5555555555555554, 0.0, 2.0, 0.0],
                             [348.666666667, 0.0, 2.25, 3.55555555556]])
-        rnum = numpy.array([[3, 3, 3, 2], 
-                            [3, 0, 3, 3], 
+        rnum = numpy.array([[3, 3, 3, 2],
+                            [3, 0, 3, 3],
                             [3, 1, 2, 3]])
         
         out = mean(inputs, masks, dof=0)
@@ -135,6 +136,44 @@ class CombineTestCase(unittest.TestCase):
             self.assertAlmostEqual(cal, precal)
         for cal, precal in zip(out[2].flat, rnum.flat):
             self.assertEqual(cal, precal)
+    
+class MinMaxTestCase(unittest.TestCase):
+    '''Test case for the minmax rejection method.'''
+    def setUp(self):
+        self.nimages = 10
+        self.data = [numpy.ones((2, 2))] * self.nimages
+        
+    def testBasic1(self):
+        '''Test value if points rejected are less than the images.'''
+        for nmin in xrange(0, self.nimages):
+            for nmax in xrange(0, self.nimages - nmin):
+                r = combine(self.data, reject='minmax', rargs=(nmin, nmax))
+            for v in r[0].flat:
+                self.assertEqual(v, 1)
+            for v in r[1].flat:
+                self.assertEqual(v, 0)
+            for v in r[2].flat:
+                self.assertEqual(v, self.nimages - nmin - nmax)
+                
+    def testBasic2(self):
+        '''Test value if points rejected are equal to the images.'''
+        for nmin in xrange(0, self.nimages):
+            nmax = self.nimages - nmin
+            r = combine(self.data, reject='minmax', rargs=(nmin, nmax))
+            for v in r[0].flat:
+                self.assertEqual(v, 0)
+            for v in r[1].flat:
+                self.assertEqual(v, 0)
+            for v in r[2].flat:
+                self.assertEqual(v, 0)
+                
+    def testBasic3(self):
+        '''Test CombineError is raised if points rejected are more than images.'''
+        for nmin in xrange(0, self.nimages):
+            nmax = self.nimages - nmin + 1
+            self.assertRaises(CombineError, combine, self.data, 
+                              reject='minmax', rargs=(nmin, nmax))
+      
       
 def test_suite():
     suite = unittest.TestSuite()
