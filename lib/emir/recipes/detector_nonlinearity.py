2564d20e7d22dfa04cc83c0c05da874525c73c88
Sergio Pascual <sergiopr@fis.ucm.es>
2010-10-07 18:31:18 +0200

    Fixed typo

--- lib/emir/recipes/detector_nonlinearity.py
+++ lib/emir/recipes/detector_nonlinearity.py
@@ -39,7 +39,7 @@ The median of the reduced flatfields is computed. For each exposure time, the
 average and standard deviation of the ADUs is computed. A linear fit is
 computed for the lowest ADU counts, with standard deviations as weights. A new
 fit is performed, this time of ADU_linear/ADU_obs, versus ADU_obs. The
-polinomyal terms of the fit are computed.
+polynomial terms of the fit are computed.
 
 '''
 
