a96acb8c45aebcd81344885710ab6a531f5d26c9
Sergio Pascual <sergiopr@fis.ucm.es>
2012-03-12 18:38:27 +0100

    Removed DiskImage

--- numina/image/__init__.py
+++ numina/image/__init__.py
@@ -25,55 +25,6 @@ import pyfits
 
 from ..array import resize_array 
 
-class DiskImage(object):
-    def __init__(self, filename):
-        super(DiskImage, self).__init__()
-        self.data = None
-        self.meta = None
-        self.filename = filename
-        self._open = False
-        self._open_extension = 'primary'
-        self.hdulist = None
-
-    def is_open(self):
-        return self._open
-
-    def open(self, mode='copyonwrite', memmap=False, extension='primary'):
-        if not self.is_open():
-            self.hdulist = pyfits.open(self.filename, memmap=memmap, mode=mode)
-            self.data = self.hdulist[extension].data
-            self.meta = self.hdulist[extension].header
-            self._open = True
-            self._open_extension = extension
-        
-    def close(self, output_verify='exception'):
-        if self.is_open():
-            self.hdulist[self._open_extension].data = self.data
-            self.hdulist.close(output_verify=output_verify)
-        self._open = False
-        self._open_extension = 'primary'
-        self.data = None
-        self.hdulist = None
-        
-    def __copy__(self):
-        new = DiskImage(filename=self.filename)
-        return new
-
-    def __str__(self):
-        return 'DiskImage(filename="%s")' % (self.filename)
-    
-    def __getstate__(self):
-        return dict(filename=self.filename)
-                
-    def __setstate__(self, data):
-        self.__init__(**data)
-        
-    def copy(self, dst):
-        shutil.copy(self.filename, dst)
-        newimg = copy.copy(self)
-        newimg.filename = dst
-        return newimg
-
 def compute_median(img, mask, region):
     d = img.data[region]
     m = mask.data[region]
