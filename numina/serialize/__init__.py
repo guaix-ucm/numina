e15d6cfb4d7849154c2265d881481544cf01e3cf
Sergio Pascual <sergiopr@fis.ucm.es>
2012-05-10 17:04:21 +0200

    Added methods to register and unregister custom serializers

--- numina/serialize/__init__.py
+++ numina/serialize/__init__.py
@@ -23,13 +23,32 @@ from collections import namedtuple
     
 SerializerInfo = namedtuple('SerializerInfo', ['name', 'dump', 'load'])
 
+_serializers = {}
+
+def register(name, dump, load):
+    '''Register a serializer by name.'''
+    global _serializers
+    _serializers[name] = (dump, load)
+    
+def unregister(name):
+    '''Unregister a serializer by name.'''
+    global _serializers
+    if name in _serializers:
+        del _serializers[name]
+
+
 def lookup(name):
     '''Lookup a serializer by name.'''
     if name == 'json':
-        from .jsonserializer import dump, load
-        return SerializerInfo('json', dump, load)
+        from .jsonserializer import dump as jdump
+        from .jsonserializer import load as jload
+        return SerializerInfo('json', jdump, jload)
     elif name == 'yaml':
-        from .yamlserializer import dump, load
-        return SerializerInfo('yaml', dump, load)
+        from .yamlserializer import dump as ydump
+        from .yamlserializer import load as yload
+        return SerializerInfo('yaml', ydump, yload)
+    elif name in _serializers:
+        dump, load = _serializers[name]
+        return SerializerInfo(name, dump, load)
     else:
         raise LookupError
