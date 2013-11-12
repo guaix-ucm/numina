040158574d99534bfd0f0bcab80c5c2f17c2c1d1
Sergio Pascual <sergiopr@fis.ucm.es>
2013-11-12 13:47:07 +0100

    Add a recipe that always fails

---
+++ numina/core/utils.py
@@ -0,0 +1,35 @@
+#
+# Copyright 2008-2013 Universidad Complutense de Madrid
+# 
+# This file is part of Numina
+# 
+# Numina is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Numina is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Numina.  If not, see <http://www.gnu.org/licenses/>.
+# 
+
+'''Recipes for system checks.  '''
+
+from .recipes import BaseRecipe
+
+class AlwaysFailRecipe(BaseRecipe):
+    '''A Recipe that always fails.'''
+
+    def __init__(self):
+        super(AlwaysFailRecipe, self).__init__(
+            author="Sergio Pascual <sergiopr@fis.ucm.es>",
+            version="0.1.0"
+        )
+
+    def run(self, observation_result, requirements):
+        raise TypeError('This Recipe always fails')
+
