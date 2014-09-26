a2698dd152c42a0f31d9d4a76912788e88d6ae08
Sergio Pascual <sergiopr@fis.ucm.es>
2014-09-26 19:13:03 +0200

    Fix PEP8 warnings

--- numina/ext/gtc/__init__.py
+++ numina/ext/gtc/__init__.py
@@ -1,7 +1,25 @@
-# 
+#
+# Copyright 2008-2014 Universidad Complutense de Madrid
+#
+# This file is part of Numina
+#
+# Numina is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Numina is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Numina.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
 import pkgutil
 
-#
 try:
     import DF
 except ImportError:
@@ -10,16 +28,15 @@ except ImportError:
     DF = types.ModuleType('DF')
     DF.TYPE_FRAME = None
 
-_eqtypes = {
-#'numina.core.products.QualityControlProduct': 'DPK::Something',
-'numina.core.products.FrameDataProduct': DF.TYPE_FRAME
-}
+_eqtypes = {'numina.core.products.FrameDataProduct': DF.TYPE_FRAME}
+
 
 def dialect_info(obj):
     key = obj.__module__ + '.' + obj.__class__.__name__
     tipo = _eqtypes.get(key, None)
     result = {'gtc': {'fqn': key, 'python': obj.python_type, 'type': tipo}}
     return result
+
 
 def register(more):
     _eqtypes.update(more)
