db9c88fdd05bc7ade46d3a53891ff0d42f7f7036
Sergio Pascual <sergiopr@fis.ucm.es>
2014-09-26 19:44:55 +0200

    Fix PEP8 errors in numina.frame

--- numina/frame/__init__.py
+++ numina/frame/__init__.py
@@ -1,31 +1,33 @@
 #
 # Copyright 2008-2014 Universidad Complutense de Madrid
-# 
+#
 # This file is part of Numina
-# 
+#
 # Numina is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Numina is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Numina.  If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 
 import warnings
 
 from astropy.io import fits
 
-from numina.array import resize_array 
+from numina.array import resize_array
+
 
 def get_hdu_shape(header):
     ndim = header['naxis']
     return tuple(header.get('NAXIS%d' % i) for i in range(1, ndim + 1))
+
 
 def custom_slice_to_str(slc):
     if slc.step is None:
@@ -33,41 +35,43 @@ def custom_slice_to_str(slc):
     else:
         return '%i:%i:%i' % (slc.start, slc.stop, slc.step)
 
+
 def custom_region_to_str(region):
     jints = [custom_slice_to_str(slc) for slc in region]
     return '[' + ','.join(jints) + ']'
 
-def resize_hdu(hdu, newshape, region, window=None, fill=0.0, scale=1, conserve=True):
+
+def resize_hdu(hdu, newshape, region, window=None, fill=0.0,
+               scale=1, conserve=True):
     basedata = hdu.data
-    newdata = resize_array(basedata, newshape, region, window=window, 
+    newdata = resize_array(basedata, newshape, region, window=window,
                            fill=fill, scale=scale, conserve=conserve)
-    hdu.header.update('NVALREGI', custom_region_to_str(region), 
+    hdu.header.update('NVALREGI', custom_region_to_str(region),
                       'Valid region of resized FITS')
     if window:
-        hdu.header.update('OVALREGI', custom_region_to_str(window), 
-                          'Valid region of original FITS')                  
-    newhdu = fits.PrimaryHDU(newdata, hdu.header)                
+        hdu.header.update('OVALREGI', custom_region_to_str(window),
+                          'Valid region of original FITS')
+    newhdu = fits.PrimaryHDU(newdata, hdu.header)
     return newhdu
+
 
 def resize_fits(fitsfile, newfilename, newshape, region, window=None,
                 scale=1, fill=0.0, clobber=True, conserve=True):
-    
+
     close_on_exit = False
     if isinstance(fitsfile, basestring):
         hdulist = fits.open(fitsfile, mode='readonly')
         close_on_exit = True
     else:
         hdulist = fitsfile
-        
+
     try:
         hdu = hdulist['primary']
-        newhdu = resize_hdu(hdu, newshape, region, fill=fill, window=window, scale=scale, conserve=conserve)
+        newhdu = resize_hdu(hdu, newshape, region, fill=fill,
+                            window=window, scale=scale, conserve=conserve)
         with warnings.catch_warnings():
             warnings.simplefilter('ignore')
             newhdu.writeto(newfilename, clobber=clobber)
     finally:
         if close_on_exit:
             hdulist.close()
-
-
-
--- numina/frame/schema.py
+++ numina/frame/schema.py
@@ -1,30 +1,31 @@
 #
 # Copyright 2014 Universidad Complutense de Madrid
-# 
+#
 # This file is part of Numina
-# 
+#
 # Numina is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Numina is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Numina.  If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 
 '''FITS header schema and validation.
 
 This module is a simplification of the FITS Schema defined
-by Erik Bray here: 
+by Erik Bray here:
 http://embray.github.io/PyFITS/schema/users_guide/users_schema.html
 
-If this schema implementation reaches pyfits/astropy stable, we will use it 
-instead of ours, with schema definitions being the same.
+If this schema implementation reaches pyfits/astropy stable,
+we will use it instead of ours, with schema definitions
+being the same.
 
 '''
 
@@ -33,9 +34,11 @@ class SchemaValidationError(Exception):
     '''Exception raised when a Schema does not validate a FITS header.'''
     pass
 
+
 class SchemaDefinitionError(Exception):
     '''Exception raised when a FITS Schema definition is not valid.'''
     pass
+
 
 def _from_ipt(value):
     if isinstance(value, bool):
@@ -59,8 +62,9 @@ def _from_ipt(value):
     else:
         raise SchemaDefinitionError(value)
 
+
 class SchemaKeyword(object):
-    def __init__(self, name, mandatory=False, valid=True, 
+    def __init__(self, name, mandatory=False, valid=True,
                  value=None):
         self.name = name
         self.mandatory = mandatory
@@ -82,7 +86,6 @@ class SchemaKeyword(object):
                 if isinstance(self.value, list):
                     self.choose = True
 
-
     def validate(self, header):
         sname = 'schema'
         # check the keyword is defined
@@ -90,19 +93,20 @@ class SchemaKeyword(object):
 
         if val is None:
             if self.mandatory:
-                raise SchemaValidationError(sname,
-                  'mandatory keyword %r missing from header' % self.name)
-            
+                raise SchemaValidationError(
+                    sname, 'mandatory keyword %r '
+                    'missing from header' % self.name)
+
             # In the rest of cases
             return True
         else:
             if not self.valid:
-                raise SchemaValidationError(sname,
-                  'invalid keyword %r present in header' % self.name)
+                raise SchemaValidationError(
+                    sname, 'invalid keyword %r present in header'
+                    % self.name)
 
         # Cases here
         # val is not None and key id mandatory or valid
-
 
         if not self.type_:
             # We dont have type information
@@ -110,45 +114,49 @@ class SchemaKeyword(object):
             return True
         else:
             if not isinstance(val, self.type_):
-                raise SchemaValidationError(sname,
-                    'keyword %r is required to have a value of type %r; got '
-                    'a value of type %r instead' %
+                raise SchemaValidationError(
+                    sname, 'keyword %r is required to have a value of type %r'
+                    '; got a value of type %r instead' %
                     (self.name, self.type_.__name__, type(val).__name__))
             # Check value
             if self.choose:
                 if val not in self.value:
-                    raise SchemaValidationError(sname,
-                      'keyword %r is required to have one of the values %r; '
-                      'got %r instead' %
-                      (self.name, self.value, val))
+                    raise SchemaValidationError(
+                        sname,
+                        'keyword %r is required to have one of the values %r; '
+                        'got %r instead' %
+                        (self.name, self.value, val))
                 else:
                     return True
             elif self.valcheck:
                 if val != self.value:
-                    raise SchemaValidationError(sname,
-                      'keyword %r is required to have the value %r; got '
-                      '%r instead' % (self.name, self.value, val))
+                    raise SchemaValidationError(
+                        sname,
+                        'keyword %r is required to have the value %r; got '
+                        '%r instead' % (self.name, self.value, val))
             else:
                 pass
 
         return True
-    
+
+
 class Schema(object):
     def __init__(self, sc):
         self.kwl = []
         self.extend(sc)
-        
+
     def validate(self, header):
         for ll in self.kwl:
             ll.validate(header)
-            
+
     def extend(self, sc):
         kw = sc['keywords']
-        for k,v in kw.items():
+        for k, v in kw.items():
             mandatory = v.get('mandatory', False)
             valid = v.get('valid', True)
             value = v.get('value', None)
-            sk = SchemaKeyword(k, mandatory=mandatory, 
-                valid=valid, value=value)
+            sk = SchemaKeyword(
+                k, mandatory=mandatory,
+                valid=valid, value=value
+                )
             self.kwl.append(sk)
-        
