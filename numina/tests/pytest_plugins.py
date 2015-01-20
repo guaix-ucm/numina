99a85d508286f4518af9bb5c866edea2b19b9a30
Sergio Pascual <sergiopr@fis.ucm.es>
2015-01-20 18:14:04 +0100

    Add a pytest fixture that downloads datasets for recipe testing

--- numina/tests/pytest_plugins.py
+++ numina/tests/pytest_plugins.py
@@ -17,7 +17,12 @@
 # along with Numina.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+import os
+import tarfile
+
 import pytest
+
+from .testcache import download_cache
 
 
 @pytest.fixture
@@ -27,6 +32,35 @@ def numinatmpdir(tmpdir):
     '''
     tmpdir.mkdir('_work')
     tmpdir.mkdir('_data')
+    return tmpdir
+
+
+@pytest.fixture
+def numinatpldir(tmpdir, request):
+    '''return a temporary directory path object
+    for numina, where a dataset has been downloaded
+    from a remote location, based on
+    the module variable BASE_URL and the test function name
+    '''
+
+    # Name of the dataset based on the function name
+    tarname = request.function.__name__[5:]
+    # Base url to donwload
+    base = getattr(request.module, 'BASE_URL')
+    url = base + tarname + '.tar.gz'
+
+    downloaded = download_cache(url)
+
+    tmpdir.chdir()
+
+    # Uncompress
+    with tarfile.open(downloaded.name, mode="r:gz") as tar:
+        tar.extractall()
+
+    os.remove(downloaded.name)
+
+    os.chdir('tpl')
+
     return tmpdir
 
 
