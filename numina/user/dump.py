785d5d64dfd5723d91d3fbb2a76ab591cb8eec9f
Sergio Pascual <sergiopr@fis.ucm.es>
2015-03-10 18:11:00 +0100

    Cleanup module

--- numina/user/dump.py
+++ numina/user/dump.py
@@ -6,10 +6,8 @@ try:
 except ImportError:
     from singledispatch import singledispatch
 
-from numina.core import RecipeResult
 
 @singledispatch
 def dump(tag, obj, where):
-    print('default impl for', type(tag))
     return obj
 
