a20e05ce502b6244fe9a88eab6aabc7f9a82aab6
Sergio Pascual <sergiopr@fis.ucm.es>
2015-06-15 12:56:57 +0200

    Remove duplicated method signature

--- numina/core/types.py
+++ numina/core/types.py
@@ -32,9 +32,6 @@ class DataType(object):
     def convert(self, obj):
         return obj
 
-    def convert(self, obj):
-        return obj
-
     def validate(self, obj):
         if not isinstance(obj, self.python_type):
             raise ValidationError(obj, self.python_type)
