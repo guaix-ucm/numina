37b6f99bb431dcd3d7ad13f333b6575fca13b5d3
Sergio Pascual <sergiopr@fis.ucm.es>
2015-06-24 19:09:56 +0200

    Change Megara by Numina

--- numina/array/trace/traces.pyx
+++ numina/array/trace/traces.pyx
@@ -1,20 +1,20 @@
 #
 # Copyright 2015 Universidad Complutense de Madrid
 #
-# This file is part of Megara DRP
+# This file is part of Numina
 #
-# Megara DRP is free software: you can redistribute it and/or modify
+# Numina is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Megara DRP is distributed in the hope that it will be useful,
+# Numina is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Megara DRP.  If not, see <http://www.gnu.org/licenses/>.
+# along with Numina.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 import cython
