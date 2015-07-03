eb1476ed8cec2d685c34d17fb640b8fe1cb3c364
Sergio Pascual <sergiopr@fis.ucm.es>
2015-07-03 13:33:29 +0200

    Obtain the trace value directly from the images

--- numina/array/trace/traces.py
+++ numina/array/trace/traces.py
@@ -18,9 +18,10 @@
 #
 
 
+from ..utils import wcs_to_pix
 from ._traces import tracing
 
-def trace(arr, x, y, p, axis=0, background=0.0,
+def trace(arr, x, y, axis=0, background=0.0,
           step=1, hs=1, tol=2, maxdis=2.0):
     '''Trace peak in array starting in (x,y).
 
@@ -34,8 +35,6 @@ def trace(arr, x, y, p, axis=0, background=0.0,
         x coordinate of the initial position
     y : float
         y coordinate of the initial position
-    p : float
-        Intensity of the initial position
     axis : {0, 1}
            Spatial axis of the array (0 is Y, 1 is X).
     background: float
@@ -49,6 +48,8 @@ def trace(arr, x, y, p, axis=0, background=0.0,
     ndarray
         A nx3 array, with x,y,p of each point in the trace
     '''
+
+    value = arr[wcs_to_pix([x, y])]
 
     # If arr is not in native byte order, the C-extension won't work
 
@@ -64,7 +65,7 @@ def trace(arr, x, y, p, axis=0, background=0.0,
     else:
         raise ValueError("'axis' must be 0 or 1")
 
-    result = tracing(arr3, x, y, p, background=background,
+    result = tracing(arr3, x, y, value, background=background,
                      step=step, hs=hs, tol=tol, maxdis=maxdis)
 
     if axis == 1:
