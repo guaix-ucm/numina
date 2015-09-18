a34201414848e3a8e7b49930b24649c405064275
Sergio Pascual <sergiopr@fis.ucm.es>
2015-09-18 17:49:36 +0200

    Function renaming and PEP8 fixes

--- numina/array/peaks/peakdet.py
+++ numina/array/peaks/peakdet.py
@@ -5,67 +5,87 @@ from scipy.ndimage.filters import generic_filter
 from ._kernels import kernel_peak_function
 
 WW = {}
-WW[3] = numpy.array([[  1.11022302e-16, 1.00000000e+00, 1.11022302e-16],
-                     [ -5.00000000e-01, 0.00000000e+00, 5.00000000e-01],
-                     [  5.00000000e-01, -1.00000000e+00, 5.00000000e-01]])
+WW[3] = numpy.array([[1.11022302e-16, 1.00000000e+00, 1.11022302e-16],
+                     [-5.00000000e-01, 0.00000000e+00, 5.00000000e-01],
+                     [5.00000000e-01, -1.00000000e+00, 5.00000000e-01]])
 
 
-def get_peak_indexes(original_data, window_width=5, threshold=0.0):
-    '''
+def find_peaks_indexes(arr, window_width=5, threshold=0.0):
+    """Find indexes of peaks in a 1d array.
 
-    :param original_data: Array
-    Has all original values of the graphic
-    :param window_width: int,
-    Width of the window to seek for the peaks.
-    :param threshold: float
-    Value used to trim values to seek for the peaks
-    :return: ndarray
-    Integer indexes of the original_data where a peak has been detected
-    '''
+    Note that window_width must be an odd number. The function imposes that the
+    fluxes in the window_width /2 points to the left (and right) of the peak
+    decrease monotonously as one moves away from the peak.
 
-    kernel_peak2 = kernel_peak_function(threshold)
-    out = generic_filter(original_data, kernel_peak2, window_width)
+    Parameters
+    ----------
+    arr : 1d numpy array
+        Input 1D spectrum.
+    window_width : int
+        Width of the window where the peak must be found. This number must be
+        odd.
+    threshold : float
+        Minimum signal in the peak (optional).
+
+    Returns
+    -------
+    ipeaks : 1d numpy array (int)
+        Indices of the input array arr in which the peaks have been found.
+
+
+    """
+
+    kernel_peak = kernel_peak_function(threshold)
+    out = generic_filter(arr, kernel_peak, window_width)
     result, =  numpy.nonzero(out)
 
     return result
 
 
 def generate_weights(window_width):
-    '''
+    """
 
     :param window_width: Int, odd number
     Width of the window (greater or equal than 3) to seek for the peaks.
     :return: ndarray
     Matrix needed to interpolate 'window_width' points
-    '''
+    """
+
     try:
         return WW[window_width]
-    except:
+    except KeyError:
         evenly_spaced = numpy.linspace(-1, 1, window_width)
         pow_matrix = numpy.vander(evenly_spaced, N=3, increasing=True)
-        final_ww =  numpy.dot(inv(numpy.dot(pow_matrix.T, pow_matrix)), pow_matrix.T)
+        final_ww = numpy.dot(inv(numpy.dot(pow_matrix.T, pow_matrix)), pow_matrix.T)
         WW[window_width] = final_ww
         return final_ww
 
 
-def refine_peaks(original_data, ipeaks, window_width):
-    '''
+def refine_peaks(arr, ipeaks, window_width):
+    """Refine the peak location previously found by find_peaks_indexes
 
-    :param original_data: array
-    Has all original values of the graphic
-    :param ipeaks: array (int)
-    Has the indexes of original_data where a peak is found
-    :param window_width: int,
-    Width of the window to seek for the peaks.
-    :return: ndarray, ndarry
-    Values which correspond with the OX and the OY axis respectively
-    '''
+    Parameters
+    ----------
+    arr : 1d numpy array, float
+        Input 1D spectrum.
+    ipeaks : 1d numpy array (int)
+        Indices of the input array arr in which the peaks were initially found.
+    window_width : int
+        Width of the window where the peak must be found.
+
+    Returns
+    -------
+     xc, yc: tuple
+        X-coordinates in which the refined peaks have been found,
+        interpolated Y-coordinates
+
+    """
 
     step = window_width // 2
 
     winoff = numpy.arange(-step, step+1)
     peakwin = ipeaks[:, numpy.newaxis] + winoff
-    ycols = original_data[peakwin]
+    ycols = arr[peakwin]
 
     ww = generate_weights(window_width)
 
@@ -76,4 +96,3 @@ def refine_peaks(original_data, ipeaks, window_width):
     xc = ipeaks + 0.5 * (window_width-1) * uc
 
     return xc, yc
-
