30619c94a5a8032b1a739a519cade53f60a74721
Sergio Pascual <sergiopr@fis.ucm.es>
2015-10-01 00:56:25 +0200

    Use fit_theil_sen from robustfit module

--- numina/array/wavecal/arccalibration.py
+++ numina/array/wavecal/arccalibration.py
@@ -11,57 +11,7 @@ from numpy.polynomial import polynomial
 import itertools
 import scipy.misc
 
-# -----------------------------------------------------------------------------
-
-
-def fit_theil_sen(x, y):
-    """Compute a robust linear fit using the Theil-Sen method.
-
-    See http://en.wikipedia.org/wiki/Theil%E2%80%93Sen_estimator for details.
-    This function "pairs up sample points by the rank of their x-coordinates
-    (the point with the smallest coordinate being paired with the first point
-    above the median coordinate, etc.) and computes the median of the slopes of
-    the lines determined by these pairs of points".
-
-    Parameters
-    ----------
-    x : 1d numpy array, float
-        X coordinate.
-    y : 1d numpy array, float
-        Y coordinate.
-
-    Returns
-    -------
-    intercept : float
-        Intercept of the linear fit.
-    slope : float
-        Slope of the linear fit.
-
-    """
-
-    if x.ndim == y.ndim == 1:
-        n = x.size
-        if n == y.size:
-            if n < 5:
-                raise ValueError('n=' + str(n) +' is < 5')
-            result = []  # python list
-            if (n % 2) == 0:
-                iextra = 0
-            else:
-                iextra = 1
-            for i in range(n//2):
-                ii = i + n//2 + iextra
-                deltax = x[ii]-x[i]
-                deltay = y[ii]-y[i]
-                result.append(deltay/deltax)
-            slope = np.median(result)
-            result = y - slope*x  # numpy array
-            intercept = np.median(result)
-            return intercept, slope
-        else:
-            raise ValueError('Invalid input sizes')
-    else:
-        raise ValueError('Invalid input dimensions')
+from ..robustfit import fit_theil_sen
 
 # -----------------------------------------------------------------------------
 
