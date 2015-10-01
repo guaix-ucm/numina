e49580789de5f8cc3081924e4e18c7e3ecd888ff
Sergio Pascual <sergiopr@fis.ucm.es>
2015-10-01 17:04:24 +0200

    Remove unused imports (logging, range)

--- numina/array/blocks.py
+++ numina/array/blocks.py
@@ -19,24 +19,19 @@
 
 from __future__ import division
 
-import logging
 from itertools import product
 
+from numpy.lib.stride_tricks import as_strided as ast
 import six
-from six.moves import range
 
 if six.PY2:
     from itertools import imap
 else:
     imap = map
 
-from numpy.lib.stride_tricks import as_strided as ast
-
-_logger = logging.getLogger("numina.array")
-
 
 def blockgen1d(block, size):
-    '''Compute 1d block intervals to be used by combine.
+    """Compute 1d block intervals to be used by combine.
 
     blockgen1d computes the slices by recursively halving the initial
     interval (0, size) by 2 until its size is lesser or equal than block
@@ -50,18 +45,18 @@ def blockgen1d(block, size):
         >>> blockgen1d(512, 1024)
         [slice(0, 512, None), slice(512, 1024, None)]
 
-    '''
-    def numblock(block, x):
-        '''Compute recursively the numeric intervals
+    """
+    def numblock(blk, x):
+        """Compute recursively the numeric intervals
 
-        '''
+        """
         a, b = x
-        if b - a <= block:
+        if b - a <= blk:
             return [x]
         else:
             result = []
             d = int(b - a) // 2
-            for i in imap(numblock, [block, block], [(a, a + d), (a + d, b)]):
+            for i in imap(numblock, [blk, blk], [(a, a + d), (a + d, b)]):
                 result.extend(i)
             return result
 
@@ -69,7 +64,7 @@ def blockgen1d(block, size):
 
 
 def blockgen(blocks, shape):
-    '''Generate a list of slice tuples to be used by combine.
+    """Generate a list of slice tuples to be used by combine.
 
     The tuples represent regions in an N-dimensional image.
 
@@ -92,16 +87,16 @@ def blockgen(blocks, shape):
         (slice(780, 1040, None), slice(0, 512, None))
         (slice(780, 1040, None), slice(512, 1024, None))
 
-    '''
+    """
     iterables = [blockgen1d(l, s) for (l, s) in zip(blocks, shape)]
     return product(*iterables)
 
 
 def blk_coverage_1d(blk, size):
-    '''Return the part of a 1d array covered by a block.
+    """Return the part of a 1d array covered by a block.
 
     :param blk: size of the 1d block
-    :param shape: size of the 1d a image
+    :param size: size of the 1d a image
     :return: a tuple of size covered and remaining size
 
     Example:
@@ -109,14 +104,14 @@ def blk_coverage_1d(blk, size):
         >>> blk_coverage_1d(7, 100)
         (98, 2)
 
-    '''
+    """
     rem = size % blk
     maxpix = size - rem
     return maxpix, rem
 
 
 def max_blk_coverage(blk, shape):
-    '''Return the maximum shape of an array covered by a block.
+    """Return the maximum shape of an array covered by a block.
 
     :param blk: the N-dimensional shape of the block
     :param shape: the N-dimensional shape of the array
@@ -128,12 +123,12 @@ def max_blk_coverage(blk, shape):
         (98, 42)
 
 
-    '''
+    """
     return tuple(blk_coverage_1d(b, s)[0] for b, s in zip(blk, shape))
 
 
 def blk_1d_short(blk, shape):
-    '''Iterate through the slices that recover a line.
+    """Iterate through the slices that recover a line.
 
     This function is used by :func:`blk_nd_short` as a base 1d case.
 
@@ -143,14 +138,14 @@ def blk_1d_short(blk, shape):
     :param blk: the size of the block
     :param shape: the size of the array
     :return: a generator that yields the slices
-    '''
+    """
     maxpix, _ = blk_coverage_1d(blk, shape)
     for i in range(0, maxpix, blk):
         yield slice(i, i + blk)
 
 
 def blk_nd_short(blk, shape):
-    '''Iterate trough the blocks that strictly cover an array.
+    """Iterate trough the blocks that strictly cover an array.
 
     Iterate trough the blocks that recover the part of the array
     given by max_blk_coverage.
@@ -179,13 +174,13 @@ def blk_nd_short(blk, shape):
           Yields blocks of blk size until the remaining part is
           smaller than `blk` and the yields smaller blocks.
 
-    '''
+    """
     internals = (blk_1d_short(b, s) for b, s in zip(blk, shape))
     return product(*internals)
 
 
 def blk_1d(blk, shape):
-    '''Iterate through the slices that recover a line.
+    """Iterate through the slices that recover a line.
 
     This function is used by :func:`blk_nd` as a base 1d case.
 
@@ -195,7 +190,7 @@ def blk_1d(blk, shape):
     :param shape: the size of the array
     :return: a generator that yields the slices
 
-    '''
+    """
     maxpix, rem = blk_coverage_1d(blk, shape)
     for i in range(0, maxpix, blk):
         yield slice(i, i + blk)
@@ -205,7 +200,7 @@ def blk_1d(blk, shape):
 
 
 def blk_nd(blk, shape):
-    '''Iterate through the blocks that cover an array.
+    """Iterate through the blocks that cover an array.
 
     This function first iterates trough the blocks that recover
     the part of the array given by max_blk_coverage
@@ -236,12 +231,12 @@ def blk_nd(blk, shape):
         :py:func:`blk_nd_short`
           Yields blocks of fixed size
 
-    '''
+    """
     internals = (blk_1d(b, s) for b, s in zip(blk, shape))
     return product(*internals)
 
 
-def block_view(A, block=(3, 3)):
+def block_view(arr, block=(3, 3)):
     """Provide a 2D block view to 2D array.
 
     No error checking made. Therefore meaningful (as implemented) only for
@@ -251,22 +246,6 @@ def block_view(A, block=(3, 3)):
 
     # simple shape and strides computations may seem at first strange
     # unless one is able to recognize the 'tuple additions' involved ;-)
-    shape = (A.shape[0] // block[0], A.shape[1] // block[1]) + block
-    strides = (block[0] * A.strides[0], block[1] * A.strides[1]) + A.strides
-    return ast(A, shape=shape, strides=strides)
-
-if __name__ == '__main__':
-    from numpy import arange
-    import six
-    A = arange(144).reshape(12, 12)
-    six.print_(block_view(A)[0, 0])
-    # [[ 0  1  2]
-    # [12 13 14]
-    # [24 25 26]]
-    six.print_(block_view(A, (2, 6))[0, 0])
-    # [[ 0  1  2  3  4  5]
-    # [12 13 14 15 16 17]]
-    six.print_(block_view(A, (3, 12))[0, 0])
-    # [[ 0  1  2  3  4  5  6  7  8  9 10 11]
-    # [12 13 14 15 16 17 18 19 20 21 22 23]
-    # [24 25 26 27 28 29 30 31 32 33 34 35]]
+    shape = (arr.shape[0] // block[0], arr.shape[1] // block[1]) + block
+    strides = (block[0] * arr.strides[0], block[1] * arr.strides[1]) + arr.strides
+    return ast(arr, shape=shape, strides=strides)
