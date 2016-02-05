405e61541af0fc0ba28394cf6bb0c73174123a3a
Sergio Pascual <sergiopr@fis.ucm.es>
2016-02-05 13:38:51 +0100

    Release v0.14

--- numina/version.py
+++ numina/version.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2015 Universidad Complutense de Madrid
+# Copyright 2015-2016 Universidad Complutense de Madrid
 #
 # This file is part of Numina
 #
@@ -19,4 +19,4 @@
 
 '''Numina version.'''
 
-version = '0.14rc3'
+version = '0.14'
