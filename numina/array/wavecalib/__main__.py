8d808900cbdd2538cd2ee58fefa97c8d6dba6b53
Nicolás Cardiel <cardiel@ucm.es>
2016-09-07 18:05:46 +0200

    Creating __main__.py script to easily compute arc calibration in a particular spectrum.

---
+++ numina/array/wavecalib/__main__.py
@@ -0,0 +1,309 @@
+#
+# Copyright 2015 Universidad Complutense de Madrid
+#
+# This file is part of Numina
+#
+# Numina is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Numina is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Numina.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""Compute the wavelength calibration of a particular spectrum."""
+
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+from astropy.io import fits
+import numpy as np
+from scipy import ndimage
+
+from .arccalibration import arccalibration
+from .arccalibration import fit_list_of_dict
+from .arccalibration import SolutionArcCalibration
+from .pause_debugplot import pause_debugplot
+from .peaks_spectrum import find_peaks_spectrum
+from .peaks_spectrum import refine_peaks_spectrum
+from .ximplot import ximplot
+
+def wvcal_spectrum(filename, ns1, ns2,
+                   nwin_background,
+                   times_sigma_threshold,
+                   wv_master_file,
+                   reverse, debugplot,
+                   poly_degree_wfit,
+                   savesp):
+    """Execute wavelength calibration of a spectrum.
+
+    The initial image can be 2 dimensional, although in this case the
+    image is supposed to have been rectified and the spectrum to be
+    calibrated will be the average of the spectra in the range
+    [ns1,ns2] (counting from 1 to NAXIS2).
+
+    Parameters
+    ----------
+    filename : string
+        File name of FITS file containing the spectra to be calibrated.
+    ns1 : int
+        First scan (from 1 to NAXIS2).
+    ns2 : int
+        Last scan (from 1 to NAXIS2).
+    nwin_background : int
+        Window size for the computation of background using a median
+        filtering with that window width. This background is computed
+        and subtracted only if this parameter is > 0.
+    times_sigma_threshold : float
+        Times robust sigma above the median to detect line peaks.
+    wv_master_file : string
+        File name of txt file containing the wavelength database.
+    reverse : bool
+        If True, reserve wavelength direction prior to wavelength
+        calibration.
+    debugplot : int
+        Determines whether intermediate computations and/or plots
+        are displayed:
+        00 : no debug, no plots
+        01 : no debug, plots without pauses
+        02 : no debug, plots with pauses
+        10 : debug, no plots
+        11 : debug, plots without pauses
+        12 : debug, plots with pauses
+    poly_degree_wfit : int
+        Degree for wavelength calibration polynomial.
+    savesp : bool
+        If True, save spectrum as xxx.fits.
+
+    """
+
+    # read FITS file
+    hdulist = fits.open(filename)
+    image_header = hdulist[0].header
+    image2d = hdulist[0].data
+    naxis2, naxis1 = image2d.shape
+    hdulist.close()
+    if debugplot >= 10:
+        print('>>> Reading file:', filename)
+        print('>>> NAXIS1:', naxis1)
+        print('>>> NAXIS2:', naxis2)
+
+    if 1 <= ns1 <= ns2 <= naxis2:
+        # extract spectrum
+        sp_mean = np.mean(image2d[(ns1-1):ns2], axis=0)
+
+        # reverse spectrum if necessary
+        if reverse:
+            sp_mean = sp_mean[::-1]
+
+        # fit and subtract background
+        if nwin_background > 0:
+            background = ndimage.filters.median_filter(sp_mean, size=81)
+            sp_mean -= background
+
+        # save spectrum in external FITS file
+        if savesp:
+            hdu = fits.PrimaryHDU(sp_mean)
+            hdu.writeto("xxx.fits", clobber=True)
+
+        # initial location of the peaks (integer values)
+        q25, q50, q75 = np.percentile(sp_mean, q=[25.0, 50.0, 75.0])
+        sigma_g = 0.7413 * (q75 - q25)  # robust standard deviation
+        threshold = q50 + times_sigma_threshold * sigma_g
+        if debugplot >= 10:
+            print("median....:", q50)
+            print("robuts std:", sigma_g)
+            print("threshold.:", threshold)
+        nwinwidth_initial = 7
+        ixpeaks = find_peaks_spectrum(sp_mean,
+                                      nwinwidth=nwinwidth_initial,
+                                      threshold=threshold)
+        # refined location of the peaks (float values)
+        nwinwidth_refined = 5
+        fxpeaks, sxpeaks = refine_peaks_spectrum(sp_mean, ixpeaks,
+                                                 nwinwidth=nwinwidth_refined,
+                                                 method="gaussian")
+
+        # print peak location and width of fitted lines
+        if debugplot >= 10:
+            print(">>> Number of lines found:", len(fxpeaks))
+            print("# line_number, channel, width")
+            for i, (fx, sx) in enumerate(zip(fxpeaks, sxpeaks)):
+                print(i, fx+1, sx)
+
+        # display median spectrum and peaks
+        if debugplot % 10 != 0:
+            title = filename
+            ax = ximplot(sp_mean, title=title, plot_bbox=(1,naxis1),
+                         show=False)
+            ymin = sp_mean.min()
+            ymax = sp_mean.max()
+            dy = ymax - ymin
+            ymin -= dy/20.
+            ymax += dy/20.
+            ax.set_ylim([ymin, ymax])
+            # display threshold
+            import matplotlib
+            matplotlib.use('Qt4Agg')
+            import matplotlib.pyplot as plt
+            plt.axhline(y=threshold, color="black", linestyle="dotted",
+                        label="detection threshold")
+            # mark peak location
+            plt.plot(ixpeaks + 1,
+                     sp_mean[ixpeaks], 'bo', label="initial location")
+            plt.plot(fxpeaks + 1,
+                     sp_mean[ixpeaks], 'go', label="refined location")
+            # legend
+            plt.legend(numpoints=1)
+            # show plot
+            plt.show(block=False)
+            plt.pause(0.001)
+            pause_debugplot(debugplot)
+
+        # read arc line wavelengths from external file
+        master_table = np.genfromtxt(wv_master_file)
+        if master_table.ndim == 1:
+            wv_master = master_table
+        else:
+            wv_master = master_table[:, 0]
+            wv_master_flag = master_table[:, 1]
+            iremove = np.where(wv_master_flag == -1)
+            if len(iremove) > 0:
+                wv_master = np.delete(wv_master, iremove)
+
+        if debugplot >= 10:
+            print("Reading master table: " + wv_master_file)
+            print("wv_master:\n", wv_master)
+
+        # wavelength calibration
+        xchannel = fxpeaks + 1.0
+        list_of_dict = arccalibration(
+            wv_master=wv_master,
+            xpos_arc=xchannel,
+            naxis1_arc=naxis1,
+            crpix1=1.0,
+            wv_ini_search=wv_master[0] - 1000.0,
+            wv_end_search=wv_master[-1] + 1000.0,
+            error_xpos_arc=3,
+            times_sigma_r=3.0,
+            frac_triplets_for_sum=0.50,
+            times_sigma_theil_sen=10.0,
+            poly_degree_wfit=poly_degree_wfit,
+            times_sigma_polfilt=10.0,
+            times_sigma_cook=10.0,
+            times_sigma_inclusion=10.0,
+            debugplot=debugplot
+        )
+
+        title = filename + "[" + str(ns1) + ":" + str(ns2) + "]" + \
+                     "\n" + wv_master_file
+        coeff, crval1_linear, crmin1_linear, crmax1_linear, cdelt1_linear = \
+            fit_list_of_dict(
+                list_of_dict=list_of_dict,
+                naxis1_arc=naxis1,
+                crpix1=1.0,
+                poly_degree_wfit=poly_degree_wfit,
+                weighted=False,
+                debugplot=12,
+                plot_title=title
+            )
+
+        # note that the class SolutionArcCalibration only stores the
+        # information in 'list_of_dict' corresponding to lines that
+        # have been properly identified
+        solution_wv = SolutionArcCalibration(
+            list_of_dict=list_of_dict,
+            coeff=coeff,
+            crpix1_linear=1.0,
+            crval1_linear=crval1_linear,
+            crmin1_linear=crmin1_linear,
+            crmax1_linear=crmax1_linear,
+            cdelt1_linear=cdelt1_linear)
+
+        # final plot with identified lines
+        ax = ximplot(sp_mean, title=title, show=False,
+                     plot_bbox=(1, naxis1))
+        ymin = sp_mean.min()
+        ymax = sp_mean.max()
+        dy = ymax-ymin
+        ymin -= dy/20.
+        ymax += dy/20.
+        ax.set_ylim([ymin, ymax])
+        # plot wavelength of each identified line
+        for xpos, wv in zip(solution_wv.xpos, solution_wv.wv):
+            ax.text(xpos, sp_mean[int(xpos+0.5)-1],
+                    str(wv), fontsize=8,
+                    horizontalalignment='center')
+        # show plot
+        import matplotlib
+        matplotlib.use('Qt4Agg')
+        import matplotlib.pyplot as plt
+        plt.show(block=False)
+        plt.pause(0.001)
+        pause_debugplot(11)
+    else:
+        raise ValueError("Invalid ns1=" + str(ns1) + ", ns2=" + str(ns2) +
+                         " values")
+
+
+if __name__ == "__main__":
+
+    # parse command-line options
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename",
+                        help="FITS image containing the spectra")
+    parser.add_argument("ns1",
+                        help="First scan (from 1 to NAXIS2)")
+    parser.add_argument("ns2",
+                        help="Last scan (from 1 to NAXIS2)")
+    parser.add_argument("wv_master_file",
+                        help="TXT file containing wavelengths")
+    parser.add_argument("nwin_background",
+                        help="window to compute background (0=none)")
+    parser.add_argument("times_sigma_threshold",
+                        help="Threshold (times sigma_g to detect lines)")
+    parser.add_argument("--debugplot",
+                        help="Integer indicating plotting/debugging" +
+                        " (default=0)",
+                        default=0)
+    parser.add_argument("--reverse",
+                        help="Reverse wavelength direction (yes/no)" +
+                        " (default=no)",
+                        default="no")
+    parser.add_argument("--degree",
+                        help="Polynomial degree (default=3)",
+                        default=3)
+    parser.add_argument("--savesp",
+                        help="Save spectrum (yes/no)" +
+                        " (default=no)",
+                        default="no")
+    args = parser.parse_args()
+
+    ns1 = int(args.ns1)
+    ns2 = int(args.ns2)
+    nwin_background = int(args.nwin_background)
+    times_sigma_threshold = float(args.times_sigma_threshold)
+    debugplot = int(args.debugplot)
+    reverse = (args.reverse == "yes")
+    poly_degree_wfit=int(args.degree)
+    savesp = (args.savesp == "yes")
+
+    wvcal_spectrum(args.filename, ns1, ns2,
+                   nwin_background,
+                   times_sigma_threshold,
+                   args.wv_master_file,
+                   reverse, debugplot,
+                   poly_degree_wfit,
+                   savesp)
+
+    try:
+        input("\nPress RETURN to QUIT...")
+    except SyntaxError:
+        pass
---
+++ numina/array/wavecalib/ximplot.py
@@ -0,0 +1,117 @@
+#
+# Copyright 2015 Universidad Complutense de Madrid
+#
+# This file is part of Numina
+#
+# Numina is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Numina is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Numina.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+
+from .pause_debugplot import pause_debugplot
+
+
+def ximplot(ycut, title=None, show=True, plot_bbox=(0, 0), debugplot=None):
+    """Auxiliary function to display 1d plot.
+
+    Parameters
+    ----------
+    ycut : 1d numpy array, float
+        Array to be displayed.
+    title : string
+        Plot title.
+    show : bool
+        If True, the function shows the displayed image. Otherwise
+        plt.show() is expected to be executed outside.
+    plot_bbox : tuple (2 integers)
+        If tuple is (0,0), the plot is displayed with image
+        coordinates (indices corresponding to the numpy array).
+        Otherwise, the bounding box of the image is read from this
+        tuple, assuming (nc1,nc2). In this case, the coordinates
+        indicate pixels.
+    debugplot : int
+        Determines whether intermediate computations and/or plots
+        are displayed:
+        00 : no debug, no plots
+        01 : no debug, plots without pauses
+        02 : no debug, plots with pauses
+        10 : debug, no plots
+        11 : debug, plots without pauses
+        12 : debug, plots with pauses
+
+    Returns
+    -------
+    ax : axes object
+        Matplotlib axes instance. This value is returned only when
+        'show' is False.
+
+    """
+
+    # protections
+    if type(ycut) is not np.ndarray:
+        raise ValueError("ycut=" + str(ycut) +
+                         " must be a numpy.ndarray")
+    elif ycut.ndim is not 1:
+        raise ValueError("ycut.ndim=" + str(ycut.dim) +
+                         " must be 1")
+
+    # read bounding box limits
+    nc1, nc2 = plot_bbox
+    plot_coord = (nc1 == 0 and nc2 == 0)
+
+    naxis1_ = ycut.size
+    if not plot_coord:
+        # check that ycut size corresponds to bounding box size
+        if naxis1_ != nc2 - nc1 + 1:
+            raise ValueError("ycut.size=" + str(ycut.size) +
+                             " does not correspond to bounding box size")
+
+    # display image
+    import matplotlib
+    matplotlib.use('Qt4Agg')
+    import matplotlib.pyplot as plt
+    # plt.ion()
+    # plt.pause(0.001)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.autoscale(False)
+    ymin = ycut.min()
+    ymax = ycut.max()
+    if plot_coord:
+        xmin = -0.5
+        xmax = (naxis1_ - 1) + 0.5
+        xcut = np.arange(naxis1_, dtype=np.float)
+        ax.set_xlabel('image array index in the X direction')
+        ax.set_ylabel('pixel value')
+    else:
+        xmin = float(nc1) - 0.5
+        xmax = float(nc2) + 0.5
+        xcut = np.linspace(start=nc1, stop=nc2, num=nc2 - nc1 + 1)
+        ax.set_xlabel('image pixel in the X direction')
+        ax.set_ylabel('pixel value')
+    ax.set_xlim([xmin, xmax])
+    ax.set_ylim([ymin, ymax])
+    ax.plot(xcut, ycut, '-')
+    if title is not None:
+        ax.set_title(title)
+    if show:
+        plt.show(block=False)
+        plt.pause(0.001)
+        pause_debugplot(debugplot)
+    else:
+        # return axes
+        return ax
