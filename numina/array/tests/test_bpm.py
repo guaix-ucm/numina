cb5dc37ecba8121efdf3af05af530069f2435312
Sergio Pascual <sergiopr@fis.ucm.es>
2016-09-08 19:59:58 +0200

    Remove floats from shape

--- numina/array/tests/test_bpm.py
+++ numina/array/tests/test_bpm.py
@@ -7,8 +7,8 @@ from numina.array.combine import mean_method
 
 def test_process_bpm():
 
-    data = numpy.zeros((10.0, 10.0), dtype='float32') + 3.0
-    mask = numpy.zeros((10.0, 10.0), dtype='int32')
+    data = numpy.zeros((10, 10), dtype='float32') + 3.0
+    mask = numpy.zeros((10, 10), dtype='int32')
     mask[3,3] = 1
 
     result = process_bpm_median(data, mask)
