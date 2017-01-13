58e8130dee2af8f8b180ae09972a0121c3ea656c
Sergio Pascual <sergiopr@fis.ucm.es>
2017-01-13 11:38:17 +0100

    Allow import_object to import modules

--- numina/core/objimport.py
+++ numina/core/objimport.py
@@ -17,20 +17,32 @@
 # along with Numina.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-'''Import objects by name'''
+"""Import objects by name"""
 
 import importlib
 import inspect
 
 
 def import_object(path):
-    '''Import an object given its fully qualified name.'''
+    """Import an object given its fully qualified name."""
     spl = path.split('.')
+    if len(spl) == 1:
+        return importlib.import_module(path)
+    # avoid last part for the moment
     cls = spl[-1]
     mods = '.'.join(spl[:-1])
+
     mm = importlib.import_module(mods)
-    Cls = getattr(mm, cls)
-    return Cls
+    # try to get the last part as an attribute
+    try:
+        obj = getattr(mm, cls)
+        return obj
+    except AttributeError:
+        pass
+
+    # Try to import the last part
+    rr = importlib.import_module(path)
+    return rr
 
 
 def fully_qualified_name(obj, sep='.'):
