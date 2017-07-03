45e670f4661f74bc65eabd7cf7fc01fbff772c66
Sergio Pascual <sergiopr@fis.ucm.es>
2017-07-03 11:19:56 +0200

    Fix typo in diskfiledal

--- numina/dal/diskfiledal.py
+++ numina/dal/diskfiledal.py
@@ -67,7 +67,7 @@ def build_product_path(drp, rootdir, conf, name, tipo, ob):
                 if fname.startswith("."):
                     _logger.debug("file %s is hidden, ignore", loadpath)
                     continue
-                if os.os.path.isfile(loadpath):
+                if os.path.isfile(loadpath):
                     _logger.debug("is regular file %s", loadpath)
                     _logger.info("found %s", loadpath)
                     return loadpath
