d732866fc956dcf76101e6e64ea96c54b0f22973
Nicolás Cardiel <cardiel@ucm.es>
2017-10-24 12:27:36 -0300

    Generate collapsed spectrum in a separate function.

--- numina/array/wavecalib/__main__.py
+++ numina/array/wavecalib/__main__.py
@@ -36,21 +36,10 @@ from .peaks_spectrum import find_peaks_spectrum
 from .peaks_spectrum import refine_peaks_spectrum
 
 
-def wvcal_spectrum(filename, ns1, ns2,
-                   poly_degree_wfit,
-                   nwin_background,
-                   times_sigma_threshold,
-                   nbrightlines,
-                   wv_master_file,
-                   reverse,
-                   out_sp,
-                   debugplot):
-    """Execute wavelength calibration of a spectrum.
-
-    The initial image can be 2 dimensional, although in this case the
-    image is supposed to have been rectified and the spectrum to be
-    calibrated will be the average of the spectra in the range
-    [ns1,ns2] (counting from 1 to NAXIS2).
+def collapsed_spectrum(filename, ns1, ns2,
+                       method='mean', nwin_background=0,
+                       reverse=False, out_sp=None, debugplot=0):
+    """Compute a collapsed spectrum from a 2D image using scans in [ns1,ns2].
 
     Parameters
     ----------
@@ -60,20 +49,13 @@ def wvcal_spectrum(filename, ns1, ns2,
         First scan (from 1 to NAXIS2).
     ns2 : int
         Last scan (from 1 to NAXIS2).
-    poly_degree_wfit : int
-        Degree for wavelength calibration polynomial.
+    method : string
+        Indicates collapsing method. Possible values are "mean" or
+        "median".
     nwin_background : int
         Window size for the computation of background using a median
         filtering with that window width. This background is computed
         and subtracted only if this parameter is > 0.
-    times_sigma_threshold : float
-        Times robust sigma above the median to detect line peaks.
-    nbrightlines : int or list of integers
-        Maximum number of brightest lines to be employed in the
-        wavelength calibration. If this value is 0, all the detected
-        lines will be employed.
-    wv_master_file : string
-        File name of txt file containing the wavelength database.
     reverse : bool
         If True, reserve wavelength direction prior to wavelength
         calibration.
@@ -82,21 +64,13 @@ def wvcal_spectrum(filename, ns1, ns2,
         performing the wavelength calibration.
     debugplot : int
         Determines whether intermediate computations and/or plots
-        are displayed:
-        00 : no debug, no plots
-        01 : no debug, plots without pauses
-        02 : no debug, plots with pauses
-        10 : debug, no plots
-        11 : debug, plots without pauses
-        12 : debug, plots with pauses
+        are displayed. The valid codes are defined in
+        numina.array.display.pause_debugplot.
 
     Returns
     -------
-    sp : numpy array (floats)
-        1d array containing the selected spectrum before computing the
-        wavelength calibration.
-    solution_wv : instance of SolutionArcCalibration
-        Wavelength calibration solution.
+    sp : 1d numpy array
+        Collapsed spectrum.
 
     """
 
@@ -112,185 +86,222 @@ def wvcal_spectrum(filename, ns1, ns2,
 
     if 1 <= ns1 <= ns2 <= naxis2:
         # extract spectrum
-        sp_mean = np.mean(image2d[(ns1-1):ns2], axis=0)
+        if method == "mean":
+            sp = np.mean(image2d[(ns1 - 1):ns2], axis=0)
+        elif method == "median":
+            sp = np.median(image2d[(ns1 - 1):ns2], axis=0)
+        else:
+            raise ValueError("Invalid method '" + str(method) + "'")
 
         # reverse spectrum if necessary
         if reverse:
-            sp_mean = sp_mean[::-1]
+            sp = sp[::-1]
 
         # fit and subtract background
         if nwin_background > 0:
-            background = ndimage.filters.median_filter(sp_mean,
-                                                       size=nwin_background)
-            sp_mean -= background
+            background = ndimage.filters.median_filter(
+                sp, size=nwin_background
+            )
+            sp -= background
 
         # save spectrum before wavelength calibration in external
         # FITS file
         if out_sp is not None:
-            hdu = fits.PrimaryHDU(sp_mean)
+            hdu = fits.PrimaryHDU(sp)
             hdu.writeto(out_sp, clobber=True)
-
-        # initial location of the peaks (integer values)
-        q50 = np.percentile(sp_mean, q=50)
-        sigma_g = robust_std(sp_mean)
-        threshold = q50 + times_sigma_threshold * sigma_g
-        if debugplot >= 10:
-            print("median....:", q50)
-            print("robuts std:", sigma_g)
-            print("threshold.:", threshold)
-        nwinwidth_initial = 7
-        ixpeaks = find_peaks_spectrum(sp_mean,
-                                      nwinwidth=nwinwidth_initial,
-                                      threshold=threshold)
-
-        # select a maximum number of brightest lines in each region
-        if len(nbrightlines) == 1 and nbrightlines[0] == 0:
-            pass
-        else:
-            region_size = (naxis1-1)/len(nbrightlines)
-            ixpeaks_filtered = np.array([], dtype=int)
-            for iregion, nlines_in_region in enumerate(nbrightlines):
-                if nlines_in_region > 0:
-                    imin = int(iregion * region_size)
-                    imax = int((iregion + 1) * region_size)
-                    if iregion > 0:
-                        imin += 1
-                    ixpeaks_region = \
-                        ixpeaks[np.logical_and(ixpeaks >= imin, ixpeaks <= imax)]
-                    if len(ixpeaks_region) > 0:
-                        peak_fluxes = sp_mean[ixpeaks_region]
-                        spos = peak_fluxes.argsort()
-                        ixpeaks_tmp = ixpeaks_region[spos[-nlines_in_region:]]
-                        ixpeaks_tmp.sort()  # in-place sort
-                        ixpeaks_filtered=np.concatenate((ixpeaks_filtered,
-                                                         ixpeaks_tmp))
-            ixpeaks = ixpeaks_filtered
-
-        # check there are enough lines for fit
-        if len(ixpeaks) <= poly_degree_wfit:
-            print(">>> Warning: not enough lines to fit spectrum computed" +
-                  " from scans [" + str(ns1) + "," + str(ns2), "]")
-            sp_mean = np.zeros(naxis1)
-            return sp_mean, None
-
-        # refined location of the peaks (float values)
-        nwinwidth_refined = 5
-        fxpeaks, sxpeaks = refine_peaks_spectrum(sp_mean, ixpeaks,
-                                                 nwinwidth=nwinwidth_refined,
-                                                 method="gaussian")
-
-        # print peak location and width of fitted lines
-        if debugplot >= 10:
-            print(">>> Number of lines found:", len(fxpeaks))
-            print("# line_number, channel, width")
-            for i, (fx, sx) in enumerate(zip(fxpeaks, sxpeaks)):
-                print(i, fx+1, sx)
-
-        # display median spectrum and peaks
-        if debugplot % 10 != 0:
-            title = filename
-            ax = ximplot(sp_mean, title=title, plot_bbox=(1, naxis1),
-                         show=False)
-            ymin = sp_mean.min()
-            ymax = sp_mean.max()
-            dy = ymax - ymin
-            ymin -= dy/20.
-            ymax += dy/20.
-            ax.set_ylim([ymin, ymax])
-            # display threshold
-            ax.axhline(y=threshold, color="black", linestyle="dotted",
-                       label="detection threshold")
-            # mark peak location
-            ax.plot(ixpeaks + 1,
-                     sp_mean[ixpeaks], 'bo', label="initial location")
-            ax.plot(fxpeaks + 1,
-                     sp_mean[ixpeaks], 'go', label="refined location")
-            # legend
-            ax.legend(numpoints=1)
-            # show plot
-            pause_debugplot(debugplot, pltshow=True)
-
-        # read arc line wavelengths from external file
-        master_table = np.genfromtxt(wv_master_file)
-        if master_table.ndim == 1:
-            wv_master = master_table
-        else:
-            wv_master_all = master_table[:, 0]
-            if master_table.shape[1] == 2:  # assume old format
-                wv_master = np.copy(wv_master_all)
-            elif master_table.shape[1] == 3:  # assume new format
-                wv_flag = master_table[:, 1]
-                wv_master = wv_master_all[np.where(wv_flag == 1)]
-            else:
-                raise ValueError('lines_catalog file does not have the '
-                                 'expected number of columns')
-
-        if debugplot >= 10:
-            print("Reading master table: " + wv_master_file)
-            print("wv_master:\n", wv_master)
-
-        wv_master_range = wv_master[-1] - wv_master[0]
-        delta_wv_master_range = 0.20 * wv_master_range
-
-        # wavelength calibration
-        xchannel = fxpeaks + 1.0
-        list_of_wvfeatures = arccalibration(
-            wv_master=wv_master,
-            xpos_arc=xchannel,
-            naxis1_arc=naxis1,
-            crpix1=1.0,
-            wv_ini_search=wv_master[0] - delta_wv_master_range,
-            wv_end_search=wv_master[-1] + delta_wv_master_range,
-            error_xpos_arc=3,
-            times_sigma_r=3.0,
-            frac_triplets_for_sum=0.50,
-            times_sigma_theil_sen=10.0,
-            poly_degree_wfit=poly_degree_wfit,
-            times_sigma_polfilt=10.0,
-            times_sigma_cook=10.0,
-            times_sigma_inclusion=10.0,
-            debugplot=debugplot
-        )
-
-        title = filename + "[" + str(ns1) + ":" + str(ns2) + "]"
-        title += "\n" + wv_master_file
-        solution_wv = fit_list_of_wvfeatures(
-            list_of_wvfeatures=list_of_wvfeatures,
-            naxis1_arc=naxis1,
-            crpix1=1.0,
-            poly_degree_wfit=poly_degree_wfit,
-            weighted=False,
-            debugplot=debugplot,
-            plot_title=title
-        )
-
-        if debugplot % 10 != 0:
-            # final plot with identified lines
-            ax = ximplot(sp_mean, title=title, show=False,
-                         plot_bbox=(1, naxis1))
-            ymin = sp_mean.min()
-            ymax = sp_mean.max()
-            dy = ymax-ymin
-            ymin -= dy/20.
-            ymax += dy/20.
-            ax.set_ylim([ymin, ymax])
-            # plot wavelength of each identified line
-            for feature in solution_wv.features:
-                xpos = feature.xpos
-                reference = feature.reference
-                ax.text(xpos, sp_mean[int(xpos+0.5)-1] + dy/100,
-                        str(reference), fontsize=8,
-                        horizontalalignment='center')
-            # show plot
-            pause_debugplot(11, pltshow=True, tight_layout=False)
-
-        # return the spectrum before the wavelength calibration and
-        # the wavelength calibration solution
-        return sp_mean, solution_wv
-
     else:
         raise ValueError("Invalid ns1=" + str(ns1) + ", ns2=" + str(ns2) +
                          " values")
+
+    return sp
+
+
+def wvcal_spectrum(sp,
+                   poly_degree_wfit,
+                   times_sigma_threshold,
+                   nbrightlines,
+                   wv_master_file,
+                   debugplot):
+    """Execute wavelength calibration of a spectrum.
+
+    Parameters
+    ----------
+    sp : 1d numpy array
+        Spectrum to be wavelength calibrated.
+    poly_degree_wfit : int
+        Degree for wavelength calibration polynomial.
+    times_sigma_threshold : float
+        Times robust sigma above the median to detect line peaks.
+    nbrightlines : int or list of integers
+        Maximum number of brightest lines to be employed in the
+        wavelength calibration. If this value is 0, all the detected
+        lines will be employed.
+    wv_master_file : string
+        File name of txt file containing the wavelength database.
+    debugplot : int
+        Determines whether intermediate computations and/or plots
+        are displayed. The valid codes are defined in
+        numina.array.display.pause_debugplot.
+
+    Returns
+    -------
+    solution_wv : instance of SolutionArcCalibration
+        Wavelength calibration solution.
+
+    """
+
+    naxis1 = sp.shape[0]
+
+    # initial location of the peaks (integer values)
+    q50 = np.percentile(sp, q=50)
+    sigma_g = robust_std(sp)
+    threshold = q50 + times_sigma_threshold * sigma_g
+    if debugplot >= 10:
+        print("median....:", q50)
+        print("robuts std:", sigma_g)
+        print("threshold.:", threshold)
+    nwinwidth_initial = 7
+    ixpeaks = find_peaks_spectrum(sp,
+                                  nwinwidth=nwinwidth_initial,
+                                  threshold=threshold)
+
+    # select a maximum number of brightest lines in each region
+    if len(nbrightlines) == 1 and nbrightlines[0] == 0:
+        pass
+    else:
+        region_size = (naxis1-1)/len(nbrightlines)
+        ixpeaks_filtered = np.array([], dtype=int)
+        for iregion, nlines_in_region in enumerate(nbrightlines):
+            if nlines_in_region > 0:
+                imin = int(iregion * region_size)
+                imax = int((iregion + 1) * region_size)
+                if iregion > 0:
+                    imin += 1
+                ixpeaks_region = \
+                    ixpeaks[np.logical_and(ixpeaks >= imin, ixpeaks <= imax)]
+                if len(ixpeaks_region) > 0:
+                    peak_fluxes = sp[ixpeaks_region]
+                    spos = peak_fluxes.argsort()
+                    ixpeaks_tmp = ixpeaks_region[spos[-nlines_in_region:]]
+                    ixpeaks_tmp.sort()  # in-place sort
+                    ixpeaks_filtered = np.concatenate((ixpeaks_filtered,
+                                                       ixpeaks_tmp))
+        ixpeaks = ixpeaks_filtered
+
+    # check there are enough lines for fit
+    if len(ixpeaks) <= poly_degree_wfit:
+        print(">>> Warning: not enough lines to fit spectrum")
+        return None
+
+    # refined location of the peaks (float values)
+    nwinwidth_refined = 5
+    fxpeaks, sxpeaks = refine_peaks_spectrum(sp, ixpeaks,
+                                             nwinwidth=nwinwidth_refined,
+                                             method="gaussian")
+
+    # print peak location and width of fitted lines
+    if debugplot >= 10:
+        print(">>> Number of lines found:", len(fxpeaks))
+        print("# line_number, channel, width")
+        for i, (fx, sx) in enumerate(zip(fxpeaks, sxpeaks)):
+            print(i, fx+1, sx)
+
+    # display median spectrum and peaks
+    if debugplot % 10 != 0:
+        ax = ximplot(sp, plot_bbox=(1, naxis1),
+                     show=False)
+        ymin = sp.min()
+        ymax = sp.max()
+        dy = ymax - ymin
+        ymin -= dy/20.
+        ymax += dy/20.
+        ax.set_ylim([ymin, ymax])
+        # display threshold
+        ax.axhline(y=threshold, color="black", linestyle="dotted",
+                   label="detection threshold")
+        # mark peak location
+        ax.plot(ixpeaks + 1, sp[ixpeaks], 'bo', label="initial location")
+        ax.plot(fxpeaks + 1, sp[ixpeaks], 'go', label="refined location")
+        # legend
+        ax.legend(numpoints=1)
+        # show plot
+        pause_debugplot(debugplot, pltshow=True)
+
+    # read arc line wavelengths from external file
+    master_table = np.genfromtxt(wv_master_file)
+    if master_table.ndim == 1:
+        wv_master = master_table
+    else:
+        wv_master_all = master_table[:, 0]
+        if master_table.shape[1] == 2:  # assume old format
+            wv_master = np.copy(wv_master_all)
+        elif master_table.shape[1] == 3:  # assume new format
+            wv_flag = master_table[:, 1]
+            wv_master = wv_master_all[np.where(wv_flag == 1)]
+        else:
+            raise ValueError('lines_catalog file does not have the '
+                             'expected number of columns')
+
+    if debugplot >= 10:
+        print("Reading master table: " + wv_master_file)
+        print("wv_master:\n", wv_master)
+
+    wv_master_range = wv_master[-1] - wv_master[0]
+    delta_wv_master_range = 0.20 * wv_master_range
+
+    # wavelength calibration
+    xchannel = fxpeaks + 1.0
+    list_of_wvfeatures = arccalibration(
+        wv_master=wv_master,
+        xpos_arc=xchannel,
+        naxis1_arc=naxis1,
+        crpix1=1.0,
+        wv_ini_search=wv_master[0] - delta_wv_master_range,
+        wv_end_search=wv_master[-1] + delta_wv_master_range,
+        error_xpos_arc=3,
+        times_sigma_r=3.0,
+        frac_triplets_for_sum=0.50,
+        times_sigma_theil_sen=10.0,
+        poly_degree_wfit=poly_degree_wfit,
+        times_sigma_polfilt=10.0,
+        times_sigma_cook=10.0,
+        times_sigma_inclusion=10.0,
+        debugplot=debugplot
+    )
+
+    title = "Wavelength calibration"
+    solution_wv = fit_list_of_wvfeatures(
+        list_of_wvfeatures=list_of_wvfeatures,
+        naxis1_arc=naxis1,
+        crpix1=1.0,
+        poly_degree_wfit=poly_degree_wfit,
+        weighted=False,
+        debugplot=debugplot,
+        plot_title=title
+    )
+
+    if debugplot % 10 != 0:
+        # final plot with identified lines
+        ax = ximplot(sp, title=title, show=False,
+                     plot_bbox=(1, naxis1))
+        ymin = sp.min()
+        ymax = sp.max()
+        dy = ymax-ymin
+        ymin -= dy/20.
+        ymax += dy/20.
+        ax.set_ylim([ymin, ymax])
+        # plot wavelength of each identified line
+        for feature in solution_wv.features:
+            xpos = feature.xpos
+            reference = feature.reference
+            ax.text(xpos, sp[int(xpos+0.5)-1] + dy/100,
+                    str(reference), fontsize=8,
+                    horizontalalignment='center')
+        # show plot
+        pause_debugplot(11, pltshow=True, tight_layout=False)
+
+    # return the wavelength calibration solution
+    return solution_wv
 
 
 def main(args=None):
@@ -310,6 +321,10 @@ def main(args=None):
                         help="window to compute background (0=none)"
                         " (default=0)",
                         default=0, type=int)
+    parser.add_argument("--method",
+                        help="collapsing method (default='mean', 'median')",
+                        default='mean',
+                        choices=['mean', 'median'])
     parser.add_argument("--times_sigma_threshold",
                         help="Threshold (times robust sigma to detect lines)"
                              " (default=10)",
@@ -348,15 +363,21 @@ def main(args=None):
     # convert nbrightlines in a list of integers
     nbrightlines = [int(nlines) for nlines in args.nbrightlines.split(",")]
 
-    wvcal_spectrum(filename=args.filename,
-                   ns1=ns1, ns2=ns2,
+    # compute collapsed spectrum
+    sp = collapsed_spectrum(
+        filename=args.filename, ns1=ns1, ns2=ns2,
+        method=args.method,
+        nwin_background=args.nwin_background,
+        reverse=args.reverse,
+        out_sp=args.out_sp,
+        debugplot=args.debugplot
+    )
+
+    wvcal_spectrum(sp=sp,
                    poly_degree_wfit=args.degree,
-                   nwin_background=args.nwin_background,
                    times_sigma_threshold=args.times_sigma_threshold,
                    nbrightlines=nbrightlines,
                    wv_master_file=args.wv_master_file,
-                   reverse=args.reverse,
-                   out_sp=args.out_sp,
                    debugplot=args.debugplot)
 
     try:
