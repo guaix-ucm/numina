0ffe3e17e699adecd48c103063942bdcd82aa0a3
Sergio Pascual <sergiopr@fis.ucm.es>
2017-11-13 12:23:17 +0100

    Fully qualified name

---
+++ numina/util/fqn.py
@@ -0,0 +1,33 @@
+#
+# Copyright 2011-2017 Universidad Complutense de Madrid
+#
+# This file is part of Numina
+#
+# Numina is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Numina is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Numina.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""Import objects by name"""
+
+import inspect
+
+
+def fully_qualified_name(obj, sep='.'):
+    """Return fully qualified name from object"""
+    if inspect.isclass(obj):
+        return obj.__module__ + sep + obj.__name__
+    else:
+        return obj.__module__ + sep + obj.__class__.__name__
+
+
+
