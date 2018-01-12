9f2fc572a8ac0779ff1a34baaa45f73cc1efdc24
Nicolás Cardiel <cardiel@ucm.es>
2018-01-12 18:57:30 +0100

    New auxiliary function to read single characters from keyboard

--- numina/array/display/iofunctions.py
+++ numina/array/display/iofunctions.py
@@ -4,6 +4,57 @@ from __future__ import print_function
 import sys
 
 
+def readc(prompt, default=None, valid=None):
+    """Return a single character read from keyboard
+
+    Parameters
+    ----------
+    prompt : str
+        Prompt string.
+    default : str
+        Default value.
+    valid : str
+        String providing valid characters. If None, all characters are
+        valid (default).
+
+    Returns
+    -------
+    cresult : str
+        Read value.
+
+    """
+
+    # main loop
+    loop = True
+    while loop:
+
+        # display prompt
+        if default is None:
+            sys.stdout.write(prompt + ' ? ')
+        else:
+            sys.stdout.write(prompt + ' [' + str(default) + '] ? ')
+
+        # read user's input
+        cresult = sys.stdin.readline().strip()
+        if cresult == '' and default is not None:
+            cresult = str(default)
+
+        if len(cresult) == 1:
+            # check that all the characters are valid
+            loop = False
+            if valid is not None:
+                for c in cresult:
+                    if c not in str(valid):
+                        print('*** Error: invalid characters found.')
+                        print('*** Valid characters are:', valid)
+                        print('*** Try again!')
+                        loop = True
+        else:
+            print('*** Error: invalid string length. Try again!')
+
+    return cresult
+
+
 def readi(prompt, default=None, minval=None, maxval=None):
     """Return integer value read from keyboard
 
@@ -128,7 +179,7 @@ def read_value(ftype, prompt, default=None, minval=None, maxval=None):
 
         # read user's input
         cresult = sys.stdin.readline().strip()
-        if cresult == '':
+        if cresult == '' and default is not None:
             cresult = default
 
         # convert to integer
@@ -141,41 +192,50 @@ def read_value(ftype, prompt, default=None, minval=None, maxval=None):
                 if result <= imaxval:
                     loop = False
                 else:
-                    print("Number out of range. Try again!")
+                    print("*** Error: number out of range. Try again!")
             elif maxval is None:
                 if result >= iminval:
                     loop = False
                 else:
-                    print("Number out of range. Try again!")
+                    print("*** Error: number out of range. Try again!")
             else:
                 if iminval <= result <= imaxval:
                     loop = False
                 else:
-                    print("Number out of range. Try again!")
+                    print("*** Error: number out of range. Try again!")
         except:
-            print("Invalid value. Try again!")
+            print("*** Error: invalid value. Try again!")
 
     return result
 
 
 def main():
 
-    i = readi("Enter integer", default=8)
+    c = readc("1 Enter character", default='y')
+    print('>>>', c)
+    c = readc("2 Enter character", default='y', valid='ynx')
+    print('>>>', c)
+    c = readc("3 Enter character", default='oso')
+    print('>>>', c)
+    c = readc("4 Enter character", default='oso', valid='yn')
+    print('>>>', c)
+
+    i = readi("1 Enter integer", default=8)
     print('>>>', i)
-    i = readi("Enter integer", minval=0)
+    i = readi("2 Enter integer", minval=0)
     print('>>>', i)
-    i = readi("Enter integer", default=7, maxval=10)
+    i = readi("3 Enter integer", default=7, maxval=10)
     print('>>>', i)
-    i = readi("Enter integer", default=101, minval=5, maxval=10)
+    i = readi("4 Enter integer", default=101, minval=5, maxval=10)
     print('>>>', i)
 
-    f = readf("Enter float", default=8)
+    f = readf("1 Enter float", default=8)
     print('>>>', f)
-    f = readf("Enter float", minval=0)
+    f = readf("2 Enter float", minval=0)
     print('>>>', f)
-    f = readf("Enter float", default=7, maxval=10)
+    f = readf("3 Enter float", default=7, maxval=10)
     print('>>>', f)
-    f = readf("Enter float", default=101, minval=5, maxval=10)
+    f = readf("4 Enter float", default=101, minval=5, maxval=10)
     print('>>>', f)
 
 
