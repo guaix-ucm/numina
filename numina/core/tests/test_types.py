6f367eb962cf794ea8c2e7a7705dc8adaeac2b3b
Sergio Pascual <sergiopr@fis.ucm.es>
2018-04-09 16:13:58 +0200

    Add compat import of 'numina.core.types.datatype' and DeprecationWarning

---
+++ numina/core/tests/test_types.py
@@ -0,0 +1,26 @@
+#
+# Copyright 2018 Universidad Complutense de Madrid
+#
+# This file is part of Numina
+#
+# Numina is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Numina is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Numina.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+def test_compat_datatype():
+
+    import numina.core.types.datatype as dcompat
+    import numina.types.datatype as mod
+
+    assert mod is dcompat
--- numina/core/types.py
+++ numina/core/types.py
@@ -1,3 +1,14 @@
 
 # Compatibility
+import sys
+import warnings
+
 from numina.types.datatype import ListOfType
+import numina.types.datatype as datatype
+
+
+warnings.warn('Module deprecated, use numina.types instead',
+              DeprecationWarning)
+
+
+sys.modules['numina.core.types.datatype'] = datatype
