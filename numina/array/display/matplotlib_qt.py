ce2a0be5e30982d4b374f8e10c8d6a0e6340fdb2
Nicolás Cardiel <cardiel@ucm.es>
2018-07-02 14:24:24 +0200

    Set logging level for matplotlib

--- numina/array/display/matplotlib_qt.py
+++ numina/array/display/matplotlib_qt.py
@@ -1,4 +1,8 @@
-import matplotlib
+import logging
+logger = logging.getLogger('matplotlib')
+logger.setLevel(logging.INFO)
+
+#import matplotlib
 #matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 plt.rcParams.update({'figure.max_open_warning': 0})  # avoid warning
