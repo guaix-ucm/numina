a8daf999bf28175da7881220e0fe4978cf996ac0
Nicolás Cardiel <cardiel@ucm.es>
2019-04-12 00:34:07 +0200

    Include test for AdaptiveLSQUnivariateSpline

--- numina/array/numsplines.py
+++ numina/array/numsplines.py
@@ -33,66 +33,81 @@ def fun_residuals(params, xnor, ynor, w, bbox, k, ext):
 class AdaptiveLSQUnivariateSpline(LSQUnivariateSpline):
     """Extend scipy.interpolate.LSQUnivariateSpline.
 
+    One-dimensional spline with explicit internal knots.
+
+    This is actually a wrapper of
+    `scipy.interpolate.LSQUnivariateSpline`
+    with the addition of using adaptive knot location determined
+    numerically (after normalising the x and y arrays before
+    the minimisation process).
+
+    Parameters
+    ----------
+    x : (N,) array_like
+        Input dimension of data points -- must be increasing
+    y : (N,) array_like
+        Input dimension of data points
+    t : (M,) array_like or int
+        When integer it indicates the number of equidistant
+        interior knots. When array_like it provides the location
+        of the interior knots of the spline; must be in ascending
+        order and::
+
+         bbox[0] < t[0] < ... < t[-1] < bbox[-1]
+
+    w : (N,) array_like, optional
+        weights for spline fitting.  Must be positive.
+        If None (default), weights are all equal.
+    bbox : (2,) array_like, optional
+        2-sequence specifying the boundary of the approximation
+        interval. If None (default), ``bbox = [x[0], x[-1]]``.
+    k : int, optional
+        Degree of the smoothing spline.  Must be 1 <= `k` <= 5.
+        Default is k=3, a cubic spline.
+    ext : int or str, optional
+        Controls the extrapolation mode for elements
+        not in the interval defined by the knot sequence.
+
+        * if ext=0 or 'extrapolate', return the extrapolated value.
+        * if ext=1 or 'zeros', return 0
+        * if ext=2 or 'raise', raise a ValueError
+        * if ext=3 of 'const', return the boundary value.
+
+        The default value is 0.
+
+    check_finite : bool, optional
+        Whether to check that the input arrays contain only finite
+        numbers, that the x array is increasing and that the
+        x and y arrays are 1-D and with the same length.
+        Disabling may give a performance gain, but may
+        result in problems (crashes, non-termination or non-sensical
+        results) if the inputs do contain infinities or NaNs.
+        Default is True.
+    adaptive : bool, optional
+        Whether to optimise knot location following the procedure
+        described in Cardiel (2009); see:
+        http://cdsads.u-strasbg.fr/abs/2009MNRAS.396..680C
+        Default is True.
+    tolerance : float, optional
+        Tolerance for Nelder-Mead minimisation process.
+
+    Attributes
+    ----------
+    _params : instance of Parameters()
+        Initial parameters before minimisation.
+    _result : Minimizer output
+        Result of the minimisation process.
+
+    See also
+    --------
+    LSQUnivariateSpline : Superclass
+
     """
 
     def __init__(self, x, y, t, w=None, bbox=(None, None),
                  k=3, ext=0, check_finite=False, adaptive=True,
                  tolerance=1E-7):
-        """One-dimensional spline with explicit internal knots.
-
-        This is actually a wrapper of
-        `scipy.interpolate.LSQUnivariateSpline`
-        with the addition of normalising the x and y arrays before
-        the fit.
-
-        Parameters
-        ----------
-        x : (N,) array_like
-            Input dimension of data points -- must be increasing
-        y : (N,) array_like
-            Input dimension of data points
-        t : (M,) array_like or int
-            When integer it indicates the number of equidistant
-            interior knots. When array_like it provides the location
-            of the interior knots of the spline; must be in ascending
-            order and::
-
-             bbox[0] < t[0] < ... < t[-1] < bbox[-1]
-
-        w : (N,) array_like, optional
-            weights for spline fitting.  Must be positive.
-            If None (default), weights are all equal.
-        bbox : (2,) array_like, optional
-            2-sequence specifying the boundary of the approximation
-            interval. If None (default), ``bbox = [x[0], x[-1]]``.
-        k : int, optional
-            Degree of the smoothing spline.  Must be 1 <= `k` <= 5.
-            Default is k=3, a cubic spline.
-        ext : int or str, optional
-            Controls the extrapolation mode for elements
-            not in the interval defined by the knot sequence.
-            * if ext=0 or 'extrapolate', return the extrapolated value.
-            * if ext=1 or 'zeros', return 0
-            * if ext=2 or 'raise', raise a ValueError
-            * if ext=3 of 'const', return the boundary value.
-            The default value is 0.
-        check_finite : bool, optional
-            Whether to check that the input arrays contain only finite
-            numbers, that the x array is increasing and that the
-            x and y arrays are 1-D and with the same length.
-            Disabling may give a performance gain, but may
-            result in problems (crashes, non-termination or non-sensical
-            results) if the inputs do contain infinities or NaNs.
-            Default is True.
-        adaptive : bool, optional
-            Whether to optimise knot location following the procedure
-            described in Cardiel (2009); see:
-            http://cdsads.u-strasbg.fr/abs/2009MNRAS.396..680C
-            Default is True.
-        tolerance : float, optional
-            Tolerance for Nelder-Mead minimisation process.
-
-        """
+        """One-dimensional spline with explicit internal knots."""
 
         if check_finite:
             # check here the arrays instead of in the base class
@@ -128,7 +143,7 @@ class AdaptiveLSQUnivariateSpline(LSQUnivariateSpline):
                     xknot[i] = (xmin + float(i + 1) * deltax)
             else:
                 xknot = np.array([])
-        except ValueError:
+        except (ValueError, TypeError):
             xknot = np.asarray(t)
             if check_finite:
                 if not np.isfinite(xknot).all():
@@ -205,3 +220,13 @@ class AdaptiveLSQUnivariateSpline(LSQUnivariateSpline):
             ext=ext,
             check_finite=False
         )
+
+    def get_params(self):
+        """Return initial parameters for minimisation process."""
+
+        return self._params
+
+    def get_result(self):
+        """Return result of minimisation process."""
+
+        return self._result
