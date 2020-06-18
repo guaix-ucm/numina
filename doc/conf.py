749bdfd704fee08ae18b320234e562f80c404329
Sergio Pascual <sergiopr@fis.ucm.es>
2020-06-19 01:05:39 +0200

    [DOCS] Update sphinx conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -62,7 +62,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Numina'
-copyright = u'2010-2019, Universidad Complutense de Madrid'
+copyright = u'2010-2020, Universidad Complutense de Madrid'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
