f43635e2662d7963fc9e0dd8da897de98ff6ee0a
Sergio Pascual <sergiopr@fis.ucm.es>
2022-11-05 02:02:39 +0100

    Use src-top layout

--- setup.py
+++ setup.py
@@ -10,17 +10,17 @@ def get_extensions():
     extensions = []
 
     ext1 = Extension('numina.array._combine',
-                     ['numina/array/src/combinemodule.cc',
-                      'numina/array/src/operations.cc',
-                      'numina/array/src/nu_combine_methods.cc',
-                      'numina/array/src/nu_combine.cc'
+                     ['src/numina/array/src/combinemodule.cc',
+                      'src/numina/array/src/operations.cc',
+                      'src/numina/array/src/nu_combine_methods.cc',
+                      'src/numina/array/src/nu_combine.cc'
                       ],
                      include_dirs=[numpy_include])
 
     extensions.append(ext1)
 
     ext3 = Extension('numina.array._nirproc',
-                     ['numina/array/src/nirproc.pyx'],
+                     ['src/numina/array/src/nirproc.pyx'],
                      include_dirs=[numpy_include],
                      language='c++'
                      )
@@ -28,31 +28,31 @@ def get_extensions():
     extensions.append(ext3)
 
     ext4 = Extension('numina.array.trace._traces',
-                     ['numina/array/trace/traces.pyx',
-                      'numina/array/trace/Trace.cpp'],
+                     ['src/numina/array/trace/traces.pyx',
+                      'src/numina/array/trace/Trace.cpp'],
                      include_dirs=[numpy_include],
                      language='c++')
     extensions.append(ext4)
 
     ext5 = Extension('numina.array.trace._extract',
-                     ['numina/array/trace/extract.pyx'],
+                     ['src/numina/array/trace/extract.pyx'],
                      include_dirs=[numpy_include],
                      language='c++')
     extensions.append(ext5)
 
     ext6 = Extension('numina.array.peaks._kernels',
-                     ['numina/array/peaks/kernels.pyx'],
+                     ['src/numina/array/peaks/kernels.pyx'],
                      language='c')
     extensions.append(ext6)
 
     ext7 = Extension('numina.array._bpm',
-                     ['numina/array/bpm.pyx'],
+                     ['src/numina/array/bpm.pyx'],
                      include_dirs=[numpy_include],
                      language='c++')
     extensions.append(ext7)
 
     ext8 = Extension('numina.array._clippix',
-                     ['numina/array/clippix.pyx'],
+                     ['src/numina/array/clippix.pyx'],
                      include_dirs=[numpy_include],
                      language='c')
     extensions.append(ext8)
