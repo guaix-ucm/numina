6a2fa8ea52720b5802a0902ce571a9194f433239
Sergio Pascual <sergiopr@fis.ucm.es>
2023-03-02 11:12:39 +0100

    Release v0.31.0

--- doc/conf.py
+++ doc/conf.py
@@ -62,14 +62,14 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Numina'
-copyright = u'2010-2020, Universidad Complutense de Madrid'
+copyright = u'2010-2023, Universidad Complutense de Madrid'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = '0.30'
+version = '0.31'
 # The full version, including alpha/beta/rc tags.
 release = '0.30.dev0'
 
