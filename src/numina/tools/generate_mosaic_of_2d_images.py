a0b6def25a62d151ed466a694ea16400eaf00567
Nicolás Cardiel <cardiel@ucm.es>
2025-02-12 19:49:09 +0100

    Refactor main() function to modularize functionality
    Moved some functionality from the main() function to a separate function within the same module. This allows for the reuse of that functionality without needing to execute the module from the terminal.

--- src/numina/tools/generate_mosaic_of_2d_images.py
+++ src/numina/tools/generate_mosaic_of_2d_images.py
@@ -7,7 +7,19 @@
 # License-Filename: LICENSE.txt
 #
 
-"""Generate a 2D mosaic from individual 2D images."""
+"""Generate a 2D mosaic from individual 2D images
+
+The images to be combined are resampled to the same WCS using the
+reproject package. Masks can be used in the combination process.
+The masks are the ones available in some extension of the individual
+images or can be calculated in real-time from the values of the
+images containing np.nan.
+
+All images resampled to the same WCS are stored in a masked array,
+allowing the combination to be performed using different functions:
+mean, median, sum, std, sigmaclip_mean, sigmaclip_median, and
+sigmaclip_stddev.
+"""
 
 import argparse
 from astropy.io import fits
@@ -23,97 +35,50 @@ REPROJECT_METHODS = ['interp', 'adaptive', 'exact']
 COMBINATION_FUNCTIONS = ['mean', 'median', 'sum', 'std','sigmaclip_mean', 'sigmaclip_median', 'sigmaclip_stddev']
 
 
-def main(args=None):
+def generate_mosaic_of_2d_images(
+        list_of_hdu2d_images,
+        list_of_hdu2d_masks=None,
+        reproject_method=None,
+        combination_function=None,
+        verbose=False
+):
+    """Combine 2D images using their WCS information.
 
-    # parse command-line options
-    parser = argparse.ArgumentParser()
-    parser.add_argument("input_list",
-                        help="TXT file with list of 2D images to be combined")
-    parser.add_argument('output_filename',
-                        help='filename of output FITS image')
-    parser.add_argument('--reproject_method',
-                        help='Reprojection method (interp, adaptive, exact)',
-                        type=str, choices=REPROJECT_METHODS, default='adaptive')
-    parser.add_argument('--extname_image',
-                        help='Extension name for image in input files. Default value: PRIMARY',
-                        default='PRIMARY', type=str)
-    parser.add_argument('--extname_mask',
-                        help='Extension name for mask in input files. Default None=use np.nan',
-                        default=None, type=str)
-    parser.add_argument("--combination_function", help='Combination function. Default: mean',
-                        type=str, default='mean', choices=COMBINATION_FUNCTIONS)
-    parser.add_argument("--output_3D_stack",
-                        help="filename for stacked 3D array. Default None",
-                        default=None, type=str)
-    parser.add_argument("--verbose",
-                        help="Display intermediate information",
-                        action="store_true")
-    parser.add_argument("--echo",
-                        help="Display full command line",
-                        action="store_true")
+    Note that this function assumes that a list of HDU images and
+    masks are available. If the masks have not been initially
+    computed, it is possible to use the function
+    'generate_mosaic_of_2d_images_without_masks()'
+    which determines a particular mask for each individual image,
+    prior to the combination, looking for np.nan values.
 
-    args = parser.parse_args(args)
+    Parameters
+    ----------
+    list_of_hdu2d_images : list
+        List of 2D HDU instances containing the images to be combined.
+    list_of_hdu2d_masks : list or None
+        List of 2D HDU instances containing the masks associated to
+        the images to be combined. If this list is None, this function
+        computes a particular mask for each 2D image prior to the
+        combination, looking for np.nan values.
+    reproject_method : str
+        Reprojection method. See 'REPROJECT_METHODS' above.
+    combination_function : str
+        Combination function. See 'COMBINATION_FUNCTIONS' above.
+    verbose : bool
+        If True, display additional information.
 
-    if args.echo:
-        print('\033[1m\033[31mExecuting: ' + ' '.join(sys.argv) + '\033[0m\n')
-
-    input_list = args.input_list
-    output_filename = args.output_filename
-    reproject_method = args.reproject_method
-    combination_function = args.combination_function
-    output_3d_stack = args.output_3D_stack
-    verbose = args.verbose
-
-    # define extensions for image and mask
-    extname_image = args.extname_image
-    extname_mask = args.extname_mask
-    if extname_mask is not None:
-        if extname_mask.lower() == 'none':
-            extname_mask = None
-
-    # read input file with list of 2D images to be combined
-    with open(input_list) as f:
-        file_content = f.read().splitlines()
-
-    # list of HDU
-    list_of_hdu2d_images = []
-    list_of_hdu2d_masks = []
-    for fname in file_content:
-        if len(fname) > 0:
-            if fname[0] not in ['#']:
-                print(f'\n* Reading: {fname}')
-                with fits.open(fname) as hdul:
-                    if extname_image not in hdul:
-                        raise ValueError(f'Expected {extname_image} extension not found')
-                    hdu2d_image = hdul[extname_image].copy()
-                    if verbose:
-                        print(f"{hdu2d_image.header['NAXIS1']=}")
-                        print(f"{hdu2d_image.header['NAXIS2']=}")
-                    if extname_mask is not None:
-                        if extname_mask in hdul:
-                            hdu2d_mask = hdul[extname_mask].copy()
-                            if verbose:
-                                print(f'image mask: {extname_mask}')
-                            bitpix_mask = hdu2d_mask.header['BITPIX']
-                            if bitpix_mask != 8:
-                                raise ValueError(f'BITPIX (mask): {bitpix_mask} is not 8')
-                            if hdu2d_image.data.shape != hdu2d_mask.data.shape:
-                                raise ValueError(f'Shape of {extname_image} and {extname_mask} are different!')
-                        else:
-                            hdu2d_mask = None
-                    else:
-                        hdu2d_mask = None
-                    # generate mask from np.nan when necessary
-                    if hdu2d_mask is None:
-                        hdu2d_mask = fits.ImageHDU(data=np.isnan(hdu2d_image.data).astype(np.uint8))
-                        hdu2d_mask.header.extend(WCS(hdu2d_image.header).to_header(), update=True)
-                        if verbose:
-                            print(f'generating mask from np.nan values')
-                    if verbose:
-                        print(f'Number of masked pixels / total: {np.sum(hdu2d_mask.data)} / {hdu2d_mask.size}')
-                    # store tuple with image and associated footprint
-                    list_of_hdu2d_images.append(hdu2d_image)
-                    list_of_hdu2d_masks.append(hdu2d_mask)
+    Returns
+    -------
+    mosaic : numpy.ma.MaskedArray
+        Masked array containing the combined image and the resulting
+        mask.
+    wcs_mosaic2d: astropy.wcs.WCS
+        WCS instance representing the World Coordinate System of the
+        combined image.
+    stack3d : numpy.ma.MaskedArray
+        Stacked masked array containing the individual reprojected
+        images and masks.
+    """
 
     nimages = len(list_of_hdu2d_images)
     if verbose:
@@ -121,6 +86,16 @@ def main(args=None):
 
     if nimages < 1:
         raise ValueError('Number of images = 0')
+
+    if list_of_hdu2d_masks is None:
+        list_of_hdu2d_masks = []
+        for hdu2d_image in list_of_hdu2d_images:
+            hdu2d_mask = fits.ImageHDU(data=np.isnan(hdu2d_image.data).astype(np.uint8))
+            hdu2d_mask.header.extend(WCS(hdu2d_image.header).to_header(), update=True)
+            list_of_hdu2d_masks.append(hdu2d_mask)
+    else:
+        if len(list_of_hdu2d_images) != len(list_of_hdu2d_masks):
+            raise ValueError(f'Unexpected {len(list_of_hdu2d_images)=} != {len(list_of_hdu2d_masks)=}')
 
     # compute optimal WCS for combined image
     wcs_mosaic2d, shape_mosaic2d = find_optimal_celestial_wcs(list_of_hdu2d_images)
@@ -226,6 +201,111 @@ def main(args=None):
 
     # set to np.nan the masked values
     mosaic.data[mosaic.mask] = np.nan
+
+    # return result
+    return mosaic, wcs_mosaic2d, stack3d
+
+
+def main(args=None):
+
+    # parse command-line options
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_list",
+                        help="TXT file with list of 2D images to be combined")
+    parser.add_argument('output_filename',
+                        help='filename of output FITS image')
+    parser.add_argument('--reproject_method',
+                        help='Reprojection method (interp, adaptive, exact)',
+                        type=str, choices=REPROJECT_METHODS, default='adaptive')
+    parser.add_argument('--extname_image',
+                        help='Extension name for image in input files. Default value: PRIMARY',
+                        default='PRIMARY', type=str)
+    parser.add_argument('--extname_mask',
+                        help='Extension name for mask in input files. Default None=use np.nan',
+                        default=None, type=str)
+    parser.add_argument("--combination_function", help='Combination function. Default: mean',
+                        type=str, default='mean', choices=COMBINATION_FUNCTIONS)
+    parser.add_argument("--output_3D_stack",
+                        help="filename for stacked 3D array. Default None",
+                        default=None, type=str)
+    parser.add_argument("--verbose",
+                        help="Display intermediate information",
+                        action="store_true")
+    parser.add_argument("--echo",
+                        help="Display full command line",
+                        action="store_true")
+
+    args = parser.parse_args(args)
+
+    if args.echo:
+        print('\033[1m\033[31mExecuting: ' + ' '.join(sys.argv) + '\033[0m\n')
+
+    input_list = args.input_list
+    output_filename = args.output_filename
+    reproject_method = args.reproject_method
+    combination_function = args.combination_function
+    output_3d_stack = args.output_3D_stack
+    verbose = args.verbose
+
+    # define extensions for image and mask
+    extname_image = args.extname_image
+    extname_mask = args.extname_mask
+    if extname_mask is not None:
+        if extname_mask.lower() == 'none':
+            extname_mask = None
+
+    # read input file with list of 2D images to be combined
+    with open(input_list) as f:
+        file_content = f.read().splitlines()
+
+    # list of HDU
+    list_of_hdu2d_images = []
+    list_of_hdu2d_masks = []
+    for fname in file_content:
+        if len(fname) > 0:
+            if fname[0] not in ['#']:
+                print(f'\n* Reading: {fname}')
+                with fits.open(fname) as hdul:
+                    if extname_image not in hdul:
+                        raise ValueError(f'Expected {extname_image} extension not found')
+                    hdu2d_image = hdul[extname_image].copy()
+                    if verbose:
+                        print(f"{hdu2d_image.header['NAXIS1']=}")
+                        print(f"{hdu2d_image.header['NAXIS2']=}")
+                    if extname_mask is not None:
+                        if extname_mask in hdul:
+                            hdu2d_mask = hdul[extname_mask].copy()
+                            if verbose:
+                                print(f'image mask: {extname_mask}')
+                            bitpix_mask = hdu2d_mask.header['BITPIX']
+                            if bitpix_mask != 8:
+                                raise ValueError(f'BITPIX (mask): {bitpix_mask} is not 8')
+                            if hdu2d_image.data.shape != hdu2d_mask.data.shape:
+                                raise ValueError(f'Shape of {extname_image} and {extname_mask} are different!')
+                        else:
+                            hdu2d_mask = None
+                    else:
+                        hdu2d_mask = None
+                    # generate mask from np.nan when necessary
+                    if hdu2d_mask is None:
+                        hdu2d_mask = fits.ImageHDU(data=np.isnan(hdu2d_image.data).astype(np.uint8))
+                        hdu2d_mask.header.extend(WCS(hdu2d_image.header).to_header(), update=True)
+                        if verbose:
+                            print(f'generating mask from np.nan values')
+                    if verbose:
+                        print(f'Number of masked pixels / total: {np.sum(hdu2d_mask.data)} / {hdu2d_mask.size}')
+                    # store tuple with image and associated footprint
+                    list_of_hdu2d_images.append(hdu2d_image)
+                    list_of_hdu2d_masks.append(hdu2d_mask)
+
+    # combine images
+    mosaic, wcs_mosaic2d, stack3d = generate_mosaic_of_2d_images(
+        list_of_hdu2d_images=list_of_hdu2d_images,
+        list_of_hdu2d_masks=list_of_hdu2d_masks,
+        reproject_method=reproject_method,
+        combination_function=combination_function,
+        verbose=verbose
+    )
 
     # save result
     hdu = fits.PrimaryHDU(mosaic.data.astype(np.float32))
