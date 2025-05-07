27423353c8d4484c86593b3c9aab5a319ef41d88
Sergio Pascual <sergiopr@fis.ucm.es>
2025-05-07 18:16:54 +0200

    Add common methods of BaseRecipe

--- src/numina/core/recipes.py
+++ src/numina/core/recipes.py
@@ -257,6 +257,82 @@ class BaseRecipe(metaclass=RecipeType):
     def extract_tags_from_ref(self, ref, tag_keys, base=None):
         return extract_tags_from_img(ref, tag_keys, self.datamodel, base=base)
 
+    def get_filters(self, imgtypes, getters):
+        import collections.abc
+
+        used_getters = []
+        for rtype, getter in zip(imgtypes, getters):
+            self.logger.debug('get_filters, %s  %s', rtype, getter)
+            if rtype is None:
+                # Unconditional
+                if isinstance(getter, collections.abc.Iterable):
+                    used_getters.extend(getter)
+                else:
+                    used_getters.append(getter)
+            else:
+                # Search
+                for key, val in self.RecipeInput.stored().items():
+                    if isinstance(val.type, rtype):
+                        if isinstance(getter, collections.abc.Iterable):
+                            used_getters.extend(getter)
+                        else:
+                            used_getters.append(getter)
+                        break
+                else:
+                    pass
+        return used_getters
+
+    def init_filters_generic(self, rinput, getters_seq, ins):
+        import numina.ext.gtc
+        import numina.util.flow as flowmod
+
+        # with BPM, bias, dark, flat and sky
+        if numina.ext.gtc.check_gtc():
+            self.logger.debug('running in GTC environment')
+        else:
+            self.logger.debug('running outside of GTC environment')
+
+        meta = self.gather_info(rinput)
+        self.logger.debug('obresult info')
+        for entry in meta['obresult']:
+            self.logger.debug('frame info is %s', entry)
+
+        reduction_flows = []
+        for getters in getters_seq:
+            correctors = [getter(rinput, meta, ins, self.datamodel)
+                          for getter in getters]
+            reduction_flow = flowmod.SerialFlow(correctors)
+            reduction_flows.append(reduction_flow)
+
+        return reduction_flows
+
+    def init_filters(self, rinput, ins=None):
+        # Defined to be overloaded
+        if ins is None:
+            ins = rinput.obresult.configuration
+
+        getters_seq = self.create_getters()
+        return self.init_filters_generic(rinput, getters_seq, ins)
+
+    def create_getters(self):
+        return []
+
+    def gather_info(self, recipeinput):
+        # Requires collaboration from datamodel
+        from numina.core import DataFrame
+
+        klass = recipeinput.__class__
+        metadata = {}
+        for key in klass.stored():
+            val = getattr(recipeinput, key)
+            if isinstance(val, DataFrame):
+                metadata[key] = self.datamodel.gather_info_dframe(val)
+            # elif isinstance(val, ObservationResult):
+            elif hasattr(val, 'frames'):
+                metadata[key] = self.datamodel.gather_info_oresult(val)
+            else:
+                pass
+        return metadata
 
 def timeit(method):
     """Decorator to measure the time used by the recipe"""
