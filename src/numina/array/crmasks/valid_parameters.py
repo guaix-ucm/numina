e6a9849779f89e79111887e65f3dfcf4145ceea3
Nicolás Cardiel <cardiel@ucm.es>
2025-10-23 05:50:32 +0200

    Employ independent file to store valid parameters

---
+++ src/numina/array/crmasks/valid_parameters.py
@@ -0,0 +1,15 @@
+#
+# Copyright 2025 Universidad Complutense de Madrid
+#
+# This file is part of Numina
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# License-Filename: LICENSE.txt
+#
+
+"""Valid parameters for cosmic ray masks computation and application."""
+
+VALID_LACOSMIC_CLEANTYPE = ['median', 'medmask', 'meanmask', 'idw']
+VALID_CRMETHODS = ['mmcosmic', 'lacosmic', 'mm_lacosmic']
+VALID_BOUNDARY_FITS = ['spline', 'piecewise']
+VALID_COMBINATIONS = ['mediancr', 'meancrt', 'meancr']
