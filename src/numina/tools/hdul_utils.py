c84eb5e99072e20a8a1cadfeb676c8671a201c58
Nicolás Cardiel <cardiel@ucm.es>
2026-09-25 20:10:21 +0200

    Change get_wcs_from_hdul to get_wcs_from_hdu

--- src/numina/tools/hdul_utils.py
+++ src/numina/tools/hdul_utils.py
@@ -50,6 +50,11 @@ def get_hdu_from_hdul(hdul, extname=None, extnum=None):
     # Determine the extension to use based on extnum or extname
     if extname is not None:
         extname_image = str(extname).upper()
+        if extname_image not in hdul:
+            logger.error(f"Extension name '{extname_image}' not found in '{hdul.filename()}'.")
+            sys.exit(1)
+        extnum_image = hdul.index_of(extname_image)
+        logger.info(f"Using extension name '{extname_image}' with number {extnum_image}.")
     else:
         if extnum < 0 or extnum > len(hdul) - 1:
             raise ValueError(f"Extension number {extnum} is out of range for the HDUList.")
@@ -57,37 +62,23 @@ def get_hdu_from_hdul(hdul, extname=None, extnum=None):
         extname_image = hdul[extnum].name
         logger.info(f"Using extension number {extnum} with name '{extname_image}'.")
 
-    # Check if the specified extension exists in the HDUList
-    if extname_image not in hdul:
-        logger.error(f"Expected '{extname_image}' extension not found")
-        sys.exit(1)
-
     # Get the HDU corresponding to the specified extension
     hdu = hdul[extname_image]
 
     return hdu
 
 
-def get_wcs_from_hdul(hdul, extname=None, extnum=None, wcskey=None):
-    """Get WCS from HDUList.
+def get_wcs_from_hdu(hdu, wcskey=None):
+    """Get WCS from HDU
 
-    The HDU can be specified either by extension name or extension number.
-    If both are provided, an error is raised. If neither is provided,
-    the first extension (0) is used by default.
-
-    Once the extension is determined, the function checks for multiple
-    WCS in the header. If multiple WCS are found, the user must specify
-    which one to use via the `wcskey` parameter. If only one WCS is found,
-    it is used directly.
+    The function checks for multiple WCS in the header. If multiple WCS are found,
+    the user must specify which one to use via the `wcskey` parameter.
+    If only one WCS is found, it is used directly.
 
     Parameters
     ----------
-    hdul : astropy.io.fits.HDUList
-        HDUList object containing the FITS file data.
-    extname : str, optional
-        Extension name for image in input files. Default is None.
-    extnum : int, optional
-        Extension number for image in input files. Default is None.
+    hdu : astropy.io.fits.ImageHDU or astropy.io.fits.PrimaryHDU
+        HDU object containing the FITS file data.
     wcskey : str, optional
         WCS key to use when multiple WCS are present in the FITS header. Default is None.
 
@@ -97,9 +88,6 @@ def get_wcs_from_hdul(hdul, extname=None, extnum=None, wcskey=None):
         WCS object corresponding to the specified extension and WCS key.
     """
     logger = logging.getLogger(__name__)
-
-    # Get the HDU from the HDUList using the provided extension name or number
-    hdu = get_hdu_from_hdul(hdul, extname=extname, extnum=extnum)
 
     # Check if the HDU is an image HDU
     if not isinstance(hdu, (fits.ImageHDU, fits.PrimaryHDU)):
